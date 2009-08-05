6785af89a4c9b85b66f4e648a6659ce2a93e94b6
Steven Knight <steven.knight@unh.edu>
2009-08-04 21:43:11 -0400

    Updated paths, removed svn usage

--- fish/.install.py
+++ fish/.install.py
@@ -9,27 +9,19 @@ from os.path import abspath, dirname
 # change to the directory our script is running in
 chdir( dirname( abspath( argv[0] ) ) )
 
-# checkout /svn/skk/SetupUser
-try:
-    retcode = call( "/usr/bin/env svn checkout https://svn.sr.unh.edu/svn/skk/SetupUser .SetupUser", shell=True)
-    if retcode < 0:
-        print "Child was terminated by signal", -retcode
-
-except OSError, e:
-        print "Execution failed:", e
-
 # add directory .SetupUser/ to python's path
-path.append( '.SetupUser' )
+path.append( '../SetupUser' )
 
 # import CreateLinks
 from createlinks import CreateLinks
 
 # create the links
 cl = CreateLinks( argv )
-# enable verbose output
-#cl.options.verbose=True
+
+# make the link
 cl.createLinks(
     [
+        { 'src': '', 'dst': '.fish' },
         { 'src': 'config.fish', 'dst': '.fishrc' },
         { 'src': 'lib/convert_csh_setup_file.py', 'dst': 'bin/convert_csh_setup_file.py' }
     ]
--- zsh/.install.py
+++ zsh/.install.py
@@ -9,17 +9,8 @@ from os.path import abspath, dirname
 # change to the directory our script is running in
 chdir( dirname( abspath( argv[0] ) ) )
 
-# checkout /svn/skk/SetupUser
-try:
-    retcode = call( "/usr/bin/env svn checkout https://svn.sr.unh.edu/svn/skk/SetupUser .SetupUser", shell=True)
-    if retcode < 0:
-        print "Child was terminated by signal", -retcode
-
-except OSError, e:
-        print "Execution failed:", e
-
 # add directory .SetupUser/ to python's path
-path.append( '.SetupUser' )
+path.append( '../SetupUser' )
 
 # import CreateLinks
 from createlinks import CreateLinks
