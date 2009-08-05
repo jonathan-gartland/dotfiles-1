620e15bfebf05572289826518c3defc58fb0d0ec
Steven Knight <steven.knight@unh.edu>
2009-08-04 21:38:49 -0400

    Added/Updated .install.py

---
+++ bin/.install.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+from sys import argv,path
+from subprocess import call
+from os import chdir
+from os.path import abspath, dirname
+
+# change to the directory our script is running in
+chdir( dirname( abspath( argv[0] ) ) )
+
+# add directory .SetupUser/ to python's path
+path.append( '../SetupUser' )
+
+# import CreateLinks
+from createlinks import CreateLinks
+
+# create the links
+cl = CreateLinks( argv )
+cl.createLinks(
+    [
+    { 'src': '', 'dst': 'bin' },
+    ] )
+
+cl 
+
