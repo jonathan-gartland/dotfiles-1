a8d4f39b150bf925c4d499503c85509bdc603d81
Steven Knight <steven.knight@unh.edu>
2009-08-10 21:43:06 -0400

    Updated how setting up the tree works.  A single python script, install.py is called from top and uses '.install.py' scripts in the children.

--- install.py
+++ install.py
@@ -1,32 +1,51 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-import sys
-import os.path
-import os
+from os import environ, getcwd, remove, symlink
+from os.path import exists, join
 
-debug = False
+def createLinks( args, options = {} ):
+    if options is None:
+        options = { 'verbose': None, 'dry_run': None }
+    else:
+        if not options.has_key('verbose'):
+            options['verbose'] = None
+        if not options.has_key('dry_rune'):
+            options['dry_run'] = None
+            
+    dst_directory = environ[ 'HOME' ]
+    src_directory = getcwd()
 
-def _execute_command( command ):
+    for arg in args:
+        print ""
+        if options[ 'verbose' ]:
+            print "argument: %s" % arg
+        
+        if arg['type'] == 'dir':
+            src = join( src_directory, arg[ 'src' ] )
+            dst = join( dst_directory, arg[ 'dst' ] )
 
-    from subprocess import call
+        if arg['type'] == 'file':
+            src = join( src_directory, arg['dir'], arg[ 'src' ] )
+            dst = join( dst_directory, arg['dir'], arg[ 'dst' ] )
 
-    try:
-        print >>sys.stderr, "Command:\n%s" % command
-        #retcode = 0
-        retcode = call(command, shell=True)
+        if exists( dst ):
+            print "Removing Link: %s" % ( dst )
+            if not options['dry_run']:
+                try:
+                    remove( dst )
+                except OSError:
+                    pass
 
-        if retcode < 0:
-            print >>sys.stderr, "Child was terminated by signal", -retcode
-
-    except OSError, e:
-            print >>sys.stderr, "Execution failed:", e
-
+        print "Creating link: %s to %s" % ( src, dst )
+        if not options['dry_run']:
+            try:
+                symlink( src, dst )
+            except OSError:
+                pass
 
 if __name__ == "__main__":
-
-    directories = ['bin', 'conky', 'dotfiles', 'emacs', 'fish', 'terminfo', 'vim', 'zsh' ]
-
-    for d in directories:
-        command = "%s/.install.py"
-        _execute_command( command )
+    for d in ['bin', 'conky', 'emacs', 'dotfiles', 'emacs', 'fish', 'terminfo', 'vim', 'zsh' ]:
+        file = "%s/.install.py" % d
+        if exists(file):
+            execfile( file )
