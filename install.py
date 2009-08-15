11c57aebb3713b658dc7aa7c5ceaa547144c5adb
Steven Knight <steven.knight@unh.edu>
2009-08-15 19:51:10 -0400

    Added submodule conky

--- install.py
+++ install.py
@@ -5,16 +5,17 @@ from os.path import exists, join
 
 def _execute_command( command ):
     from subprocess import call
+    from sys import stderr
     try:
-        print >>sys.stderr, "Command:\n%s" % command
+        print >>stderr, "Command:\n%s" % command
         #retcode = 0
         retcode = call(command, shell=True)
 
         if retcode < 0:
-            print >>sys.stderr, "Child was terminated by signal", -retcode
+            print >>stderr, "Child was terminated by signal", -retcode
 
     except OSError, e:
-            print >>sys.stderr, "Execution failed:", e
+            print >>stderr, "Execution failed:", e
 
 def createLinks( args, options = {} ):
     if options is None:
@@ -24,7 +25,7 @@ def createLinks( args, options = {} ):
             options['verbose'] = None
         if not options.has_key('dry_rune'):
             options['dry_run'] = None
-            
+
     dst_directory = environ[ 'HOME' ]
     src_directory = getcwd()
 
@@ -52,9 +53,11 @@ def createLinks( args, options = {} ):
                 pass
 
 if __name__ == "__main__":
-    for dir in ['bin', 'conky', 'emacs', 'dotfiles', 'emacs', 'fish', 'terminfo', 'vim', 'zsh' ]:
+    for dir in ['bin', 'conky', 'emacs', 'dotfiles', 'emacs', 'fish', 'vim']:
+
+        _execute_command( "git submodule init %s" % dir )
+        _execute_command( "git submodule update %s" % dir )
+
         file = "%s/.install.py" % dir
         if exists(file):
-	    _execute_command( "git init %s" % dir ):
-	    _execute_command( "git update %s" % dir ):
             execfile(file)
