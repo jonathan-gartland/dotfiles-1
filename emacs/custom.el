21e27d08f5cfd1d73cda5f7f67c14fab0be989e4
Steven Knight <skk@sr.unh.edu>
2009-09-01 19:42:55 -0400

    Added sqlite settings

--- emacs/custom.el
+++ emacs/custom.el
@@ -6,7 +6,9 @@
  '(flymake-gui-warnings-enabled nil)
  '(flymake-log-level 0)
  '(inhibit-startup-screen t)
- '(ispell-personal-dictionary "~/.emacs.d/aspell.dict"))
+ '(ispell-personal-dictionary "~/.emacs.d/aspell.dict")
+ '(sql-product (quote sqlite))
+ '(sql-sqlite-program "sqlite3"))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
