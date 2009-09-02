13978417cc519c8b2bbeaec05412e7ed6f8a1fa0
Steven Knight <steven.knight@unh.edu>
2009-09-01 21:34:37 -0400

    Added creation of eg sym-link

--- bin/.install.py
+++ bin/.install.py
@@ -4,4 +4,8 @@
 from install import createLinks
 
 # create the links
-createLinks( [ { 'src': 'bin', 'dst': 'bin' } ], { 'dry_run' : True, 'verbose' : False } )
+createLinks( [ 
+{ 'src': 'bin', 'dst': 'bin' },
+{ 'src': 'bin/.eg.git/eg', 'dst': 'bin/eg' }
+], 
+{ 'dry_run' : False, 'verbose' : False } )
