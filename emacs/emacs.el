99ffb8990029b584292b006629717b50199b1124
Steven Knight <steven.knight@unh.edu>
2009-09-13 21:25:54 -0400

    Added better flyspell support

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -550,6 +550,17 @@ directory, select directory. Lastly the file is opened."
 ; flyspell
 (require 'flyspell)
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(flyspell-mode 1)
+(add-hook 'c++-mode-common-hook 'turn-on-flyspell)
+(add-hook 'c++-mode-hook 'turn-on-flyspell)
+(add-hook 'c-mode-common-hook 'turn-on-flyspell)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-flyspell)
+(add-hook 'fundamental-mode-hook 'turn-on-flyspell)
+(add-hook 'message-mode-hook 'turn-on-flyspell)
+(add-hook 'python-mode-hook 'turn-on-flyspell)
+(add-hook 'text-mode-hook 'turn-on-flyspell)
+(defun turn-on-flyspell ()
+  "Force flyspell-mode on using a positive argument. For use in hooks."
+  (interactive)
+  (flyspell-mode 1))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
