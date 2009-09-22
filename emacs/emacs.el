93ddfa08f1a7e3ae8b3fc9811a9f32dc2f49116f
Steven Knight <steven.knight@unh.edu>
2009-09-22 18:31:26 -0400

    Added created of .wl.  Added initial wanderlust config

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -579,3 +579,33 @@ directory, select directory. Lastly the file is opened."
 ; Section  Template
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; wanderlust
+(defconst wl-testing 'nil)
+(when wl-testing 
+    (add-to-list 'load-path "/usr/share/emacs/site-lisp/")
+    (add-to-list 'load-path "/usr/share/emacs/site-lisp/apel")
+    (add-to-list 'load-path "/usr/share/emacs/site-lisp/flim")
+    (add-to-list 'load-path "/usr/share/emacs/site-lisp/semi")
+    (add-to-list 'load-path "/usr/share/emacs/site-lisp/wl")
+
+    ;; mel-b-ccl is buggy. use emacs built-in base64 decoder
+    (setq mel-b-ccl-module nil
+          base64-internal-decoding-limit nil)
+    (require 'mel-b-el)
+    (defalias 'base64-internal-decode-string 'base64-decode-string)
+
+    (autoload 'wl "wl" "wanderlust" t)
+    (autoload 'wl-draft "wl" "Write draft with Wanderlust." t)
+    (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+    (setq wl-icon-directory "/usr/share/pixmaps/wl/")
+
+    ; enable ssl
+    (setq elmo-imap4-default-stream-type 'ssl)
+    ; set stmp server
+    (setq wl-smtp-posting-server "blackstar.sr.unh.edu")
+    ; set imap server
+    (setq elmo-imap4-default-server "blackstar.sr.unh.edu"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
