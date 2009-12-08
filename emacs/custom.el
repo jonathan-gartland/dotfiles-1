4b3ec4302b3ba68e96ca1a4bb18bf915a29a7491
Steven Knight <skk@sr.unh.edu>
2009-12-08 17:51:38 -0500

    Added changing of cursor color and auto-revert-interval

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(auto-revert-interval 1)
  '(flymake-gui-warnings-enabled nil)
  '(flymake-log-level 0)
  '(inhibit-startup-screen t)
