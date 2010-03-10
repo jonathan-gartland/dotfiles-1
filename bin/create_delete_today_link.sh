d7f392b70321d62667dd9b286fd5ca9324a9f107
Steven Knight <skk@sr.unh.edu>
2010-03-10 15:02:55 -0500

    Updated today link

--- bin/create_delete_today_link.sh
+++ bin/create_delete_today_link.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-/usr/bin/env mkdir $HOME/delete/$(/usr/bin/env date +%F) 2> /dev/null
+/usr/bin/env rm -f $HOME/delete/today
+/usr/bin/env mkdir  $HOME/delete/$(/usr/bin/env date +%F) 
 /usr/bin/env ln -sf $HOME/delete/$(/usr/bin/env date +%F) $HOME/delete/today
