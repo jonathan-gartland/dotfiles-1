70ff2b1ae62c51a1ebeed20433edb9aee8f3780c
Steven Knight <skk@sr.unh.edu>
2010-03-10 15:03:47 -0500

    Updated Ham maildirs

--- bin/LearnSpam.sh
+++ bin/LearnSpam.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Ham Maildir
-sa-learn --no-sync --ham $HOME/Maildir/.Archive/{cur,new} > /dev/null
+sa-learn --no-sync --ham $HOME/Maildir/.MaildirInbox/{cur,new} > /dev/null
+sa-learn --no-sync --ham $HOME/Maildir/.Ops/{cur,new} > /dev/null
 
 # Spam Maildir
 sa-learn --no-sync --spam $HOME/Maildir/.Junk/{cur,new} > /dev/null
