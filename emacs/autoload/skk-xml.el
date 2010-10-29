30884143bbc10ae7c00a94e11e32c733656b4986
Steven Knight <skk@sr.unh.edu>
2010-10-29 13:39:50 -0400

    Update auto-mode-alist

--- emacs/autoload/skk-xml.el
+++ emacs/autoload/skk-xml.el
@@ -1,5 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq auto-mode-alist
-	  (append '(("\\.xml$" . nxml-mode)) auto-mode-alist))
+	  (append '(("\\.xml$" . nxml-mode)) auto-mode-alist)
+	  (append '(("\\.jsp$" . nxml-mode)) auto-mode-alist))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'skk-xml)
