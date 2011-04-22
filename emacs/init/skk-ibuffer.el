b232e8d2921209b5d23e284e9756418b5dce3e15
Steven Knight <skk@sr.unh.edu>
2011-04-22 16:50:14 -0400

    Added UPS, EPSCoR, NHIHPP

--- emacs/init/skk-ibuffer.el
+++ emacs/init/skk-ibuffer.el
@@ -9,10 +9,50 @@
              (filename . "/web/nh-wetlands-mapper-dev"))
             ("Housing"
              (filename . "/web/housing"))
+            ("EPSCoR"
+             (or
+              (filename . "/web/epscor")
+              (filename . "/web/perl/Lib/EPSCOR")))
+            ("UPS"
+             (filename . "~/work/ups"))
             ("Emacs Config"
-             (filename . "~/.emacs.d/"))
+              (filename . "emacs"))
+            ("NHIHPP"
+             (filename . "/web/perl/Lib/NHIHPP"))
             ("DotFilesForest"
              (filename . "dot-files-forest"))
+            ("version control" 
+             (or (mode . svn-status-mode)
+                 (mode . svn-log-edit-mode)
+                 (name . "^\\*svn-")
+                 (name . "^\\*vc\\*$")
+                 (name . "^\\*Annotate")
+                 (name . "^\\*git-")
+                 (name . "^\\*vc-")))
+            ("emacs" 
+             (or 
+              (name . "^\\*scratch\\*$")
+              (name . "^\\*Messages\\*$")
+              (name . "^TAGS\\(<[0-9]+>\\)?$")
+              (name . "^\\*Help\\*$")
+              (name . "^\\*info\\*$")
+              (name . "^\\*Occur\\*$")
+              (name . "^\\*grep\\*$")
+              (name . "^\\*Compile-Log\\*$")
+              (name . "^\\*Backtrace\\*$")
+              (name . "^\\*Process List\\*$")
+              (name . "^\\*gud\\*$")
+              (name . "^\\*Man")
+              (name . "^\\*WoMan")
+              (name . "^\\*Customize\*")
+              (name . "^\\*Pymacs\*")
+              (name . "^\\*Kill Ring\\*$")
+              (name . "^\\*Completions\\*$")
+              (name . "^\\*tramp")
+              (name . "^\\*Quail\*")
+              (name . "^\\*pymacs run")
+              (name . "^\\*shell\\*$")
+              (name . "^\\*compilation\\*$")))
             ("Programming" ;; prog stuff not already in MyProjectX
               (or
                 (mode . c-mode)
@@ -23,10 +63,12 @@
                 ))
             ))))
 
+(add-hook 'ibuffer-mode-hook 
+	  '(lambda ()
+	     (ibuffer-auto-mode 1)
+	     (ibuffer-switch-to-saved-filter-groups "default")))
 
-(add-hook 'ibuffer-mode-hook
-  (lambda ()
-    (ibuffer-switch-to-saved-filter-groups "default")))
+(setq ibuffer-show-empty-filter-groups nil)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
