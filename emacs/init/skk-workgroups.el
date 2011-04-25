386911c30a1c8287b23e563c0d8e0fb2611cad83
Steven Knight <skk@sr.unh.edu>
2011-04-25 15:11:51 -0400

    updated to load wg data but NOT auto switch

--- emacs/init/skk-workgroups.el
+++ emacs/init/skk-workgroups.el
@@ -3,7 +3,8 @@
 
 (require 'workgroups)
 (workgroups-mode 1)
-;(wg-load "~/.emacs.d/init/.workgroups")
+(setq wg-switch-on-load nil) ; don't auto switch to the first workgroup
+(wg-load "~/.emacs.d/init/.workgroups")
 
 (provide 'skk-workgroups)
 ;;; skk-workgroups.el ends here
