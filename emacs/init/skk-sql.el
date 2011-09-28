59b1fcc103c183c52a7a6ccedfa6ec1404635b6f
Steven Knight <steven@knight.cx>
2011-09-28 16:52:07 -0400

    commented out skk-sql.el code

--- emacs/init/skk-sql.el
+++ emacs/init/skk-sql.el
@@ -1,15 +1,17 @@
 
-(require 'sqlplus)
-(require 'sql)
-(add-to-list 'auto-mode-alist '("\\.sqp\\'" . sqlplus-mode))
-
-(defun install-eat-sqlplus-junk ()
-  "Install `comint-preoutput-filter-functions' if appropriate.
-Add this function to `sql-interactive-mode-hook' in your .emacs:
-\(add-hook 'sql-mode-hook 'install-eat-sqlplus-junk)"
-  (if (string= (car (process-command (get-buffer-process sql-buffer)))
-               sql-oracle-program)
-      (add-to-list 'comint-preoutput-filter-functions
-                   'eat-sqlplus-junk)))
-
-(add-hook 'sql-interactive-mode-hook 'install-eat-sqlplus-junk)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (require 'sqlplus)                                                     ;;
+;; (require 'sql)                                                         ;;
+;; (add-to-list 'auto-mode-alist '("\\.sqp\\'" . sqlplus-mode))           ;;
+;;                                                                        ;;
+;; (defun install-eat-sqlplus-junk ()                                     ;;
+;;   "Install `comint-preoutput-filter-functions' if appropriate.         ;;
+;; Add this function to `sql-interactive-mode-hook' in your .emacs:       ;;
+;; \(add-hook 'sql-mode-hook 'install-eat-sqlplus-junk)"                  ;;
+;;   (if (string= (car (process-command (get-buffer-process sql-buffer))) ;;
+;;                sql-oracle-program)                                     ;;
+;;       (add-to-list 'comint-preoutput-filter-functions                  ;;
+;;                    'eat-sqlplus-junk)))                                ;;
+;;                                                                        ;;
+;; (add-hook 'sql-interactive-mode-hook 'install-eat-sqlplus-junk)        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
