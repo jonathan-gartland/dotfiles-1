b7a1a248324444794e866967aecaa9da184e236f
Steven Knight <steven@knight.cx>
2012-01-11 11:04:22 -0500

    Updated tramp-default-methd, tool-bar-mode

--- emacs/custom.el
+++ emacs/custom.el
@@ -1,8 +1,8 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(auto-revert-interval 1)
  '(browse-url-browser-function (quote browse-url-generic))
  '(browse-url-generic-program "/opt/google/chrome/google-chrome")
@@ -22,11 +22,12 @@
  '(sql-sqlite-program "sqlite3")
  '(tab-always-indent nil)
  '(term-unbind-key-list (quote ("C-x" "C-c" "C-h" "C-y" "<ESC>")))
- '(tramp-default-method "ssh" t)
+ '(tool-bar-mode nil)
+ '(tramp-default-method "ssh")
  '(viper-shift-width 4))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Anonymous Pro")))))
