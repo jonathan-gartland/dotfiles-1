b11f61f7494a8bf05697e7a35b7e5413691872a9
Steven Knight <steven@knight.cx>
2012-01-11 11:06:46 -0500

    Added new files.

---
+++ emacs/init/skk-kdb-macro.el
@@ -0,0 +1,3 @@
+(fset 'nec-format-tr
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 60 116 114 13 6 return tab 19 60 127 47 116 100 62 13 tab return tab 19 116 100 62 13 tab return tab 19 19 6 2 return tab 19 19 2 6 return tab 19 19 13 tab return tab 19 19 13 tab return tab 19 19 6 2 return tab 19 19 13 return 32 32 32 32 14] 0 "%d")) arg)))
+
