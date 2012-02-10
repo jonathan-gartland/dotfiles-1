7e2a1ff1b7d18547390c56214a0343857fdf4dd1
Steven Knight <steven@knight.cx>
2012-02-09 21:02:40 -0500

    Removed template text.  Added require for added themes.

--- emacs/init/skk-colortheme.el
+++ emacs/init/skk-colortheme.el
@@ -1,26 +1,3 @@
-;;; skk-colortheme.el --- ColorTheme
-
-;; Copyright (C) 2009  skk
-
-;; Author: skk <skk@localhost.localdomain>
-;; Keywords: 
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; 
 
 (when (require 'color-theme)
   (require 'color-theme-github)
@@ -34,6 +11,8 @@
   (require 'color-theme-sanityinc-solarized)
   (require 'color-theme-actress)
   (require 'color-theme-dg)
+  (require 'color-theme-molokai)
+  (require 'color-theme-cobalt)
   (require 'color-theme-complexity) 
   (require 'color-theme-wombat)
                                         ;(require 'color-theme-wombat+)
@@ -74,4 +53,3 @@
 
 
 (provide 'skk-colortheme)
-;;; skk-colortheme.el ends here
