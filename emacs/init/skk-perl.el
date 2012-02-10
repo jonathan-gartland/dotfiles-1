edb9c0b3e42a48370b966ce907248afb491b9c01
Steven Knight <steven@knight.cx>
2012-02-09 21:01:23 -0500

    Updates to get flymake/syntax checking working in perl; though we're lot there yet.

--- emacs/init/skk-perl.el
+++ emacs/init/skk-perl.el
@@ -12,15 +12,27 @@
  (setq cperl-indent-level 2)
 )
 
+(defun my-cperl-eldoc-documentation-function ()
+      "Return meaningful doc string for `eldoc-mode'."
+      (car
+       (let ((cperl-message-on-help-error nil))
+         (cperl-get-help))))
+
+
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (set (make-local-variable 'eldoc-documentation-function)
+                 'my-cperl-eldoc-documentation-function)))
+
 ;; (defun flymake-perl-init ()
 ;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
 ;;                      'flymake-create-temp-inplace))
 ;;      	 (local-file (file-relative-name
 ;;                       temp-file
 ;;                       (file-name-directory buffer-file-name))))
-;;     (list "/usr/local/bin/perl_syntax_checker_emacs.sh" (list " " local-file))))
+;;     (list "/sshfs/lithium/usr/local/bin/perl_syntax_checker.pl" (list " " local-file))))
 
-;; (setq flymake-log-level 5)
+(setq flymake-log-level 10)
 
 ;; (setq flymake-allowed-file-name-masks nil)
 
@@ -37,10 +49,32 @@
 ;;                2 3 nil 1)
 ;;              flymake-err-line-patterns))
 
-(add-hook 'cperl-mode-hook
-          '(lambda ()
-             (yas/minor-mode-on)
-             (setq yas/mode-symbol 'perl-mode)))
+;; (add-hook 'cperl-mode-hook
+;;           '(lambda ()
+;;              (yas/minor-mode-on)
+;;              (setq yas/mode-symbol 'perl-mode)))
+
+(defun flymake-perl-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+     	 (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list perl-command (list perl-dbg-flags temp-file))))
+
+;; (setq flymake-allowed-file-name-masks
+;;       (cons '(".+\\pm$"
+;;               flymake-perl-init
+;;               flymake-simple-cleanup
+;;               flymake-get-real-file-name)))
+
+;; (setq flymake-err-line-patterns
+;;       (cons '("\\(.*\\) at \\([^ \n]+\\) line \\([0-9]+\\)[,.\n]"
+;;               2 3 nil 1)
+;;             flymake-err-line-patterns))
+
 
 (provide 'skk-perl)
 ;;; skk-perl.el ends here
+
+
