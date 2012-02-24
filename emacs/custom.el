5dbd4efde59db494ec4fe87ca90aa72035dcf72e
Steven Knight <skk@sr.unh.edu>
2012-02-24 16:59:31 -0500

    Updated

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,12 +4,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-revert-interval 1)
+ '(blink-cursor-mode nil)
  '(browse-url-browser-function (quote browse-url-generic))
  '(browse-url-generic-program "/opt/google/chrome/google-chrome")
  '(c-default-style (quote ((c-mode . "k&r") (c++-mode . "k&r") (java-mode . "java") (awk-mode . "awk"))))
  '(column-number-mode t)
+ '(custom-safe-themes (quote ("7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" "d14db41612953d22506af16ef7a23c4d112150e5" default)))
  '(flymake-gui-warnings-enabled nil)
  '(flymake-log-level 0 t)
+ '(icicle-mode t)
  '(inhibit-startup-screen t)
  '(ispell-personal-dictionary "~/.emacs.d/aspell.dict")
  '(mk-proj-ack-cmd "~/bin/ack")
@@ -19,6 +22,7 @@
  '(regex-tool-backend (quote perl))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
+ '(size-indication-mode t)
  '(sql-product (quote oracle))
  '(sql-sqlite-program "sqlite3")
  '(tab-always-indent nil)
@@ -31,4 +35,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Anonymous Pro")))))
+ '(default ((t (:inherit nil :stipple nil :background "#181818" :foreground "#e4e4ef" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Inconsolata")))))
