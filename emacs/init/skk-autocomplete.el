489abfeb5d098307d69f593691d5101dca82ff25
Steven Knight <steven@knight.cx>
2012-04-18 18:31:02 -0400

    Fixed auto-completion setup

--- emacs/init/skk-autocomplete.el
+++ emacs/init/skk-autocomplete.el
@@ -1,10 +1,12 @@
+
 ;;; skk-autocomplete.el --- AutoComplete
 
 ;(add-to-list 'load-path "~/.emacs.d/elisp/auto-complete")
-;(require 'auto-complete-config)
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
+
+(require 'auto-complete-config)
+(ac-config-default)
 ;(setq ac-comphist-file  "~/.emacs.d/.cache/ac-comphist.dat")
-;(ac-config-default)
+;(add-to-list 'ac-dictionary-directories "~/.emacs.d/.cache/ac-dict")
 
 (provide 'skk-autocomplete)
 ;;; skk-autocomplete.el ends here
