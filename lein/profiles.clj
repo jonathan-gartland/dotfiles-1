17dd7290ca3b69619b78d3e545b9930cda25dce8
Steven Knight <steven@knight.cx>
2012-05-02 19:13:31 -0400

    Added plugin lein-autodoc

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,6 +3,7 @@
         :plugins [
             [lein-exec "0.2.0"]
             [lein-swank "1.4.4"]
+            [lein-autodoc "0.9.0"]
             [org.clojure/clojure "1.3.0"]
         ] 
     }
