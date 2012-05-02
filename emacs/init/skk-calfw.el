cb9e659513d11b873b910aba80821141b363e38b
Steven Knight <steven@knight.cx>
2012-05-02 19:11:18 -0400

    Added themes and calfw

---
+++ emacs/init/skk-calfw.el
@@ -0,0 +1,9 @@
+(require 'calfw)
+(require 'calfw-cal)
+(require 'calfw-ical)
+
+; private ical
+(cfw:open-ical-calendar "https://www.google.com/calendar/ical/02388ju54civ56dklbv8n8tihk%40group.calendar.google.com/private-7937a20b31137f5ab34376f3bfe7b4a0/basic.ics")
+(cfw:open-ical-calendar "https://www.google.com/calendar/ical/stevenknight1980%40gmail.com/private-8016020c8aaedfdd7a86000c09f786e9/basic.ics")
+
+(provide 'skk-calfw)
