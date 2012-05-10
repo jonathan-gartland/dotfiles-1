9622eeffc91d4fd3f77c433eda9849d2f680c677
Steven Knight <steven@knight.cx>
2012-05-10 12:10:50 -0400

    Removed lein-autodoc plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,7 +3,6 @@
         :plugins [
             [lein-exec "0.2.0"]
             [lein-swank "1.4.4"]
-            [lein-autodoc "0.9.0"]
             [org.clojure/clojure "1.3.0"]
         ] 
     }
