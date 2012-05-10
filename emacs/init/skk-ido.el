c386a0f08bd2396145ff523d4859feb2d8ad732e
Steven Knight <steven@knight.cx>
2012-05-10 12:10:58 -0400

    Updated ido

--- emacs/init/skk-ido.el
+++ emacs/init/skk-ido.el
@@ -2,8 +2,16 @@
 
 (when (require-maybe 'ido)
     (ido-mode t)
+    (setq ido-create-new-buffer (quote never))
+    (setq ido-enable-flex-matching t)
+    (setq ido-enable-last-directory-history nil)
+    (setq ido-enable-regexp nil)
+    (setq ido-max-directory-size 300000)
+    (setq ido-max-file-prompt-width 0.1)
+    (setq ido-use-filename-at-point (quote guess))
+    (setq ido-use-url-at-point t)
+    (setq ido-use-virtual-buffers t)
     (setq ido-enable-flex-matching t)) ;; enable fuzzy matching
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; http://emacsrocks.com/e10.html
