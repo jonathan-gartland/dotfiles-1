3c4f436bc78b5eec0153b10a1b969aa596f14c36
Steven Knight <steven@knight.cx>
2012-06-18 18:07:56 -0400

    Updated theme

--- emacs/custom.el
+++ emacs/custom.el
@@ -5,16 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(auto-revert-interval 1)
- '(blink-cursor-mode nil)
  '(browse-url-browser-function (quote browse-url-generic))
  '(browse-url-generic-program "/opt/google/chrome/google-chrome")
  '(c-default-style (quote ((c-mode . "k&r") (c++-mode . "k&r") (java-mode . "java") (awk-mode . "awk"))))
  '(column-number-mode t)
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" "d14db41612953d22506af16ef7a23c4d112150e5" default)))
- '(custom-theme-load-path (quote ("/home/skk/.emacs.d/el-get/" custom-theme-directory t "~/.emacs.d/themes")))
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes (quote ("36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" "a7816e280688829bde537faf2a1ec736d383ce6d97e39e1439d65d153143c2fa" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" "d14db41612953d22506af16ef7a23c4d112150e5" default)))
+ '(custom-theme-load-path (quote ("/home/skk/.emacs.d/el-get/" custom-theme-directory t "~/.emacs.d/themes" "~/.emacs.d/el-get/theme-sinburn")))
  '(flymake-gui-warnings-enabled nil)
  '(flymake-log-level 0 t)
+ '(font-use-system-font t)
  '(icicle-mode t)
  '(inhibit-startup-screen t)
  '(ispell-personal-dictionary "~/.emacs.d/aspell.dict")
@@ -30,11 +30,10 @@
  '(tab-always-indent nil)
  '(term-unbind-key-list (quote ("C-x" "C-c" "C-h" "C-y" "<ESC>")))
  '(tool-bar-mode nil)
- '(tramp-default-method "ssh")
- '(viper-shift-width 4))
+ '(tramp-default-method "ssh"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#181818" :foreground "#e4e4ef" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Inconsolata")))))
+ )
