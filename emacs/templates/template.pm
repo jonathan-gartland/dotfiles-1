36f0bc428284817ca028be3143697abd0716fbca
Steven Knight <steven@knight.cx>
2012-08-02 19:01:13 -0400

    Added template for Perl

---
+++ emacs/templates/template.pm
@@ -0,0 +1,15 @@
+package XXX;
+
+use strict;
+use warnings "all";
+
+use Data::Dumper;
+
+use Exporter;
+use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
+$VERSION = "1.0";
+@ISA = ('Exporter');
+@EXPORT = qw();
+
+
+1;
