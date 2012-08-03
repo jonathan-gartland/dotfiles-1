40a34602bb296e66eafe5c4c3c93debb8d93c829
Steven Knight <steven@knight.cx>
2012-08-03 17:38:47 -0400

    Set default theme, tango-dark

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,17 +4,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(auto-insert-mode t)
  '(auto-revert-interval 1)
+ '(background-color "#fdf6e3")
+ '(background-mode light)
+ '(bookmark-save-flag t)
  '(browse-url-browser-function (quote browse-url-generic))
  '(browse-url-generic-program "/opt/google/chrome/google-chrome")
  '(c-default-style (quote ((c-mode . "k&r") (c++-mode . "k&r") (java-mode . "java") (awk-mode . "awk"))))
  '(column-number-mode t)
- '(custom-enabled-themes (quote (monokai)))
+ '(cursor-color "#657b83")
+ '(custom-enabled-themes (quote (tango-dark)))
  '(custom-safe-themes (quote ("36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" "a7816e280688829bde537faf2a1ec736d383ce6d97e39e1439d65d153143c2fa" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "7fe1e3de3e04afc43f9a3d3a8d38cd0a0efd9d4c" "d14db41612953d22506af16ef7a23c4d112150e5" default)))
  '(custom-theme-load-path (quote ("/home/skk/.emacs.d/el-get/" custom-theme-directory t "~/.emacs.d/themes" "~/.emacs.d/el-get/theme-sinburn")))
+ '(desktop-path (quote ("~/.emacs.d/.cache/")))
+ '(display-time-mode t)
  '(flymake-gui-warnings-enabled nil)
- '(flymake-log-level 0 t)
+ '(flymake-log-level 0)
  '(font-use-system-font t)
+ '(foreground-color "#657b83")
  '(icicle-mode t)
  '(inhibit-startup-screen t)
  '(ispell-personal-dictionary "~/.emacs.d/aspell.dict")
@@ -30,7 +38,7 @@
  '(tab-always-indent nil)
  '(term-unbind-key-list (quote ("C-x" "C-c" "C-h" "C-y" "<ESC>")))
  '(tool-bar-mode nil)
- '(tramp-default-method "ssh"))
+ '(tramp-default-method "ssh" t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
