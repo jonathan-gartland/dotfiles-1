cedefdf11184490a340f579f746892d22b38f273
Steven Knight <steven@knight.cx>
2012-08-06 19:45:26 -0400

    Commented out flymake use for python

--- emacs/init.el
+++ emacs/init.el
@@ -1227,16 +1227,17 @@ Symbols matching the text at point are put first in the completion list."
                                     (show-paren-mode 1)
                                     (eldoc-mode 1))))
 
-    (when (load "flymake" t)
-      (defun flymake-pychecker-init ()
-        (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                           'flymake-create-temp-inplace))
-               (local-file (file-relative-name
-                            temp-file
-                            (file-name-directory buffer-file-name))))
-          (list "pychecker"  (list local-file))))
-      (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-pychecker-init)))
-    (add-hook 'find-file-hook 'flymake-find-file-hook)))
+    ;(when (load "flymake" t)
+    ;  (defun flymake-pychecker-init ()
+    ;    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+    ;                       'flymake-create-temp-inplace))
+    ;           (local-file (file-relative-name
+    ;                        temp-file
+    ;                        (file-name-directory buffer-file-name))))
+    ;      (list "pychecker"  (list local-file))))
+    ;  (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-pychecker-init)))
+    ;(add-hook 'find-file-hook 'flymake-find-file-hook)
+    ))
 
 
 ;;;_. ack
