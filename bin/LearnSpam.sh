978109f557a2b0e368682fe687daa16d90975d4e
Steven Knight <steven.knight@unh.edu>
2012-08-14 23:23:57 -0400

    Corrected mailboxes used

--- bin/LearnSpam.sh
+++ bin/LearnSpam.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Ham Maildir
-sa-learn --no-sync --ham $HOME/Maildir/.MaildirInbox/{cur,new} > /dev/null
+sa-learn --no-sync --ham $HOME/Maildir/.INBOX/{cur,new} > /dev/null
 
 # Spam Maildir
 sa-learn --no-sync --spam $HOME/Maildir/.Junk/{cur,new} > /dev/null
+sa-learn --no-sync --spam $HOME/Maildir/.Spam/{cur,new} > /dev/null
