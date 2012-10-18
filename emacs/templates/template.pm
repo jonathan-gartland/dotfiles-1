f36ee244fdf2b1f079c77c58f523b7761f505df1
Steven Knight <skk@sr.unh.edu>
2012-10-18 19:00:51 -0400

    Updated perl template

--- emacs/templates/template.pm
+++ emacs/templates/template.pm
@@ -2,14 +2,14 @@ package XXX;
 
 use strict;
 use warnings "all";
-
 use Data::Dumper;
-
 use Exporter;
 use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
 $VERSION = "1.0";
 @ISA = ('Exporter');
 @EXPORT = qw();
+
+use Apache2::Const qw(:common);
 
 
 1;
