5e01b6c4c077411847c47891dfec0717a8cd1eee
Steven Knight <skk@sr.unh.edu>
2012-11-06 19:58:59 -0500

    Added new, execute methods.  Updated imports.

--- emacs/templates/template.pm
+++ emacs/templates/template.pm
@@ -3,13 +3,39 @@ package XXX;
 use strict;
 use warnings "all";
 use Data::Dumper;
+use Carp;
 use Exporter;
-use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
-$VERSION = "1.0";
-@ISA = ('Exporter');
-@EXPORT = qw();
-
 use Apache2::Const qw(:common);
+use SQL::Abstract;
+# use Lib::HCGS::AppLog::ApplicationLog;
+# use Lib::HCGS::Util;
 
+use base 'Exporter';
+our $VERSION = "1.0";
+our @EXPORT_OK = qw();
+
+sub new {
+    my $class  = shift;
+    my $self   = bless {}, $class;
+    my $module = __PACKAGE__;
+
+    my $args = shift
+      or croak "$module No Argument object provided!";
+
+    $self->{args} = $args;
+    ($self->{r}, $self->{dbh}, $self->{priv}, $self->{form}, $self->{html}, $self->{js}) = $args->allArgs();
+    $self->{form_name} = '';
+    $self->{sql} = SQL::Abstract->new();
+    # $self->{applog} = Lib::HCGS::AppLog::ApplicationLog->new($args);
+    # $self->{util} = Lib::HCGS::Util->new($self->{args});
+    return $self;
+}
+
+sub execute {
+    my ($self) = @_;
+}
+
+sub handler {
+}
 
 1;
