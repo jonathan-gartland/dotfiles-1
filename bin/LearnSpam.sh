c20dae2571247dfcbcb591d92c3980484d58bd1a
Steven Knight <skk@sr.unh.edu>
2012-11-27 19:42:15 -0500

    Corrected paths.

--- bin/LearnSpam.sh
+++ bin/LearnSpam.sh
@@ -5,4 +5,3 @@ sa-learn --no-sync --ham $HOME/Maildir/.INBOX/{cur,new} > /dev/null
 
 # Spam Maildir
 sa-learn --no-sync --spam $HOME/Maildir/.Junk/{cur,new} > /dev/null
-sa-learn --no-sync --spam $HOME/Maildir/.Spam/{cur,new} > /dev/null
