5b988a970af6015cb41910af1c9e4ad8b3872878
Steven Knight <steven@knight.cx>
2013-01-03 21:18:44 -0500

    Disable recentf and indicate-empty-lines

--- emacs/sane-defaults.el
+++ emacs/sane-defaults.el
@@ -50,8 +50,8 @@
 (setq fill-column 80)
 
 ;; Save a list of recent files visited. (open recent file with C-x f)
-;(recentf-mode 1j)
-;(setq recentf-max-saved-items 100) ;; just 20 is too recent
+(recentf-mode 1)
+(setq recentf-max-saved-items 100) ;; just 20 is too recent
 
 ;; Undo/redo window configuration with C-c <left>/<right>
 (winner-mode 1)
@@ -60,7 +60,7 @@
 (set-default 'indent-tabs-mode nil)
 
 ;; Show me empty lines after buffer end
-(set-default 'indicate-empty-lines t)
+;(set-default 'indicate-empty-lines t)
 
 ;; Easily navigate sillycased words
 (global-subword-mode 1)
