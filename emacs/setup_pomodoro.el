66a4a74f2d35a51f274d7038f071cf7975788c25
Steven Knight <skk@sr.unh.edu>
2013-01-11 20:43:11 -0500

    Corrected bug in loading pomodoro

--- emacs/setup_pomodoro.el
+++ emacs/setup_pomodoro.el
@@ -1,2 +1,7 @@
-(require 'pomodoro)
+(use-package pomodoro)
+(setq pomodoro-work-time 27
+      pomodoro-short-break 3
+      pomodoro-long-break 10
+      pomodoro-raise-frame nil)
+
 (provide 'setup_pomodoro)
