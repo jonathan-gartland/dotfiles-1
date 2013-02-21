fed2fc9105d39b6266b2b937ce8823ea8d9d1a6b
Steven Knight <steven@knight.cx>
2013-02-21 18:19:29 -0500

    Added

---
+++ emacs/setup_evil.el
@@ -0,0 +1,9 @@
+(use-package evil
+  :config
+  (progn
+    (evil-mode 1)
+    (setq evil-insert-state-cursor '("red" hbar))
+    ))
+
+(provide 'setup_evil)
+;;; setup_evil.el ends here
---
+++ emacs/setyp_flycheck.el
@@ -0,0 +1,1 @@
+(provide 'setyp_flycheck)
