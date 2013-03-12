678040fc6190df322ce9f5f8209c427932d7b31a
Steven Knight <steven.knight@unh.edu>
2013-03-11 21:44:23 -0400

    Moved files.

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,3 @@
-;; Turn off mouse interface early in startup to avoid momentary display
-;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-;; (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 (setq is-mac (equal system-type 'darwin))
@@ -33,199 +28,12 @@
 (add-to-list 'load-path (expand-file-name "~/dot-files-forest/use-package"))
 (require 'use-package)
 (require 'bind-key)
-(defun sacha/package-install (package &optional repository)
-  "Install PACKAGE if it has not yet been installed.
-If REPOSITORY is specified, use that."
-  (unless (package-installed-p package)
-    (let ((package-archives (if repository
-                                (list (assoc repository package-archives))
-                              package-archives)))
-    (package-install package))))
-;; (use-package el-get
-;;   :disabled t
-;;   :commands (el-get
-;;              ;; el-get-install
-;;              ;; el-get-update
-;;              ;; el-get-list-packages
-;;              )
-;;   :init
-;;   (defvar el-get-sources nil)
-
-;;   :config
-;;   (defun el-get-read-status-file ()
-;;     (mapcar #'(lambda (entry)
-;;                 (cons (plist-get entry :symbol)
-;;                       `(status "installed" recipe ,entry)))
-;;             el-get-sources))
-
-;;   (defalias 'el-get-init 'ignore
-;;     "Don't use el-get for making packages available for use."))
-
 
 ; common lisp goodies, loop
 (require 'cl)			
 
-;; (unless (require 'el-get nil t)
-;;    (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el" 
-;;                  (lambda (s) 
-;;                    (let (el-get-master-branch) (end-of-buffer) (eval-print-last-sexp)))))
-
-;; (setq el-get-verbose t)
-;; (require 'el-get)
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/recipes")
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/vendor/el-get-recipes")
-
-;; now either el-get is `require'd already, or have been `load'ed by the
-;; el-get installer.
-
-;(el-get-emacswiki-refresh)
-
-;; (setq el-get-sources '(
-;; 		       (:name bookmark-plus :type github :pkgname "emacsmirror/bookmark-plus")
-;;                        ;; (:name icicles :type emacswiki)
-;;                        ;; (:name icicles-mac :type emacswiki)
-;;                        ;; (:name icicles-face :type emacswiki)
-;;                        ;; (:name icicles-opt :type emacswiki)
-;;                        ;; (:name icicles-var :type emacswiki)
-;;                        ;; (:name icicles-fn :type emacswiki)
-;;                        ;; (:name icicles-mcmd :type emacswiki)
-;;                        ;; (:name icicles-cmd1 :type emacswiki)
-;;                        ;; (:name icicles-cmd2 :type emacswiki)
-;;                        ;; (:name icicles-doc1 :type emacswiki)
-;;                        ;; (:name icicles-doc2 :type emacswiki)
-;;                        ;; (:name icicles-mode :type emacswiki)
-;; ))
-
-;; (if (string-match "linux" system-configuration)
-;;     (loop for p in '(emacs-w3m magit swank-clojure) ;auctex pymacs rope ropemacs slime swank-clojure
-;;           do (add-to-list 'el-get-sources p)))
-
-;; (setq my-el-get-packages  
-;;       (append  
-;;        '(
-;;          ace-jump-mode
-;;          ack
-;;          auto-complete
-;;          auto-complete-css
-;;          auto-complete-emacs-lisp
-;;          auto-complete-etags
-;;          auto-complete-extension
-;;          auto-complete-yasnippet
-;;          autopair
-;;          base16-theme
-;;          bookmark-plus
-;;          ;; bookmark+-1
-;;          ;; bookmark+-bmu
-;;          ;; bookmark+-doc
-;;          ;; bookmark+-key
-;;          ;; bookmark+-mac
-;;          ;; bookmark+-lit
-;;          boxquote
-;;          buffer-move
-;;          ; breadcrumb
-;;          calfw
-;;          clevercss
-;;          clojure-mode
-;;          cperl-mode
-;;          ;crontab-mode
-;;          coffee-mode
-;;          color-theme-solarized
-;; ;         company
-;;          csv
-;;          csv-mode
-;;          dtrt-indent
-;; ;         dictionary
-;; ;         dict
-;;          durendal
-;;          ;edit-server
-;;          expand-region
-;;          fill-column-indicator
-;;          flymake-perlcritic
-;;          flymake-cursor
-;;          ;git-emacs
-;;          ;gitconfig-mode
-;;          ;git-modeline
-;;          geiser
-;; ;         google-maps
-;; ;         google-weather
-;;          haskell-mode
-;;          helm
-;;          hexview-mode
-;; ;         hexrgb
-;;          hide-region
-;;          hl-line+
-;;          idomenu
-;;          js2-mode
-;;          jshint-mode
-;;          ;js2-refactorr 
-;;          json
-;;          jump-char        
-;;          lua-mode
-;;          lusty-explorer
-;;          key-chord
-;;          offlineimap
-;;          mapserver-mode
-;;          mark-multiple
-;;          markdown-mode
-;;          multiple-cursors
-;;          multi-term
-;;          shell-pop
-;;          mk-project
-;;          mu4e
-;;          ;newsticker-notify
-;;          nrepl
-;;          notify
-;;          ;org-mode
-;;          package
-;;          paredit
-;;          point-stack
-;; ;         powerline2
-;; ;         python
-;; ;         python-mode
-;;          python_mode_fgallina
-;;          pretty-symbols-mode
-;;          pomodoro
-;; ;; python-pep8
-;;          quack
-;;          rainbow-delimiters
-;;          rainbow-mode
-;; ;         redo
-;;          rebox2
-;;          rect-mark
-;;          sinburn-theme
-;;          smart-tab
-;;          smex
-;;          sql
-;;          sql-complete
-;;          sqlplus
-;;          smooth-scrolling
-;;          swank-clojure
-;;          sunrise-commander
-;;          sunrise-x-checkpoints
-;;          sunrise-x-loop
-;;          sunrise-x-mirror
-;;          sunrise-x-modeline
-;;          sunrise-x-tree
-;;          ;tomorrow-night-paradise-theme
-;;          tiling
-;;          tail
-;;          undo-tree
-;;          xclip
-;;          yasnippet
-;;          whole-line-or-region
-;;          zencoding-mode
-;;          window-number
-;;          workgroups)    
-;;        (mapcar 'el-get-source-name el-get-sources)))
-
-;; (el-get 'sync my-el-get-packages)
-
-;(require 'package)
-
 ;; Add marmalade to package repos
-;; (defun init-install-packages ())
 (require 'package)
-;(require 'melpa)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
@@ -238,23 +46,14 @@ If REPOSITORY is specified, use that."
   '(
     ace-jump-mode
     ack
-    ;; auto-complete
-    ;; auto-complete-css
-    ;; auto-complete-emacs-lisp
-    ;; auto-complete-etags
-    ;; auto-complete-extension
-    ;; auto-complete-yasnippet
-    ;autopair
-    base16-theme
-;    bookmark-plus
     boxquote
     buffer-move
-;    clevercss
     clojure-mode
     cperl-mode
     coffee-mode
     color-theme-solarized
     csv-mode
+    csharp-mode
     dtrt-indent
     durendal
     expand-region
@@ -445,25 +244,12 @@ If REPOSITORY is specified, use that."
            (add-to-list 'load-path p) 
            (cd p) (normal-top-level-add-subdirs-to-load-path)) vendor-load-path)
 
-;; ;; Browse kill ring
-;; (require 'browse-kill-ring)
-;; (setq browse-kill-ring-quit-action 'save-and-restore)
-
-;; Misc
-;;(require 'appearance)
-;; (require 'my-misc)
-
-;; ;; Elisp go-to-definition with M-. and back again with M-,
-;; (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
-;; (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
-;; (eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
-
 ;; ;; Emacs server
 (require 'server)
 (unless (server-running-p)
   (server-start))
 
-;; ;; Run at full power please
+;; Run at full power please
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 
@@ -471,7 +257,7 @@ If REPOSITORY is specified, use that."
 ;; (require 'diminish)
 ;; (diminish 'yas/minor-mode)
 
-(when (file-exists-p user-emacs-directory)
+(when (file-exists-p (concat user-emacs-directory "lib"))
   (mapc 'load (directory-files user-emacs-directory t "^setup_.*el$")))
 
 ;; Keep emacs Custom-settings in separate file
