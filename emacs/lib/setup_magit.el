cf4f8408ce67a00e1336ca065e68a2071529afed
Steven Knight <steven.knight@unh.edu>
2013-05-07 17:05:40 -0400

    Added bind-key for magit-status.

---
+++ emacs/lib/setup_magit.el
@@ -0,0 +1,6 @@
+(use-package magit
+  :init
+  (progn
+    (bind-key "C-x C-g" 'magit-status)))
+
+(provide 'setup_magit)
---
+++ emacs/lib/setupo_smartparens.el
@@ -0,0 +1,4 @@
+(smartparens-global-mode)
+(use-package smartparens-config)
+
+(provide 'setupo_smartparens)
