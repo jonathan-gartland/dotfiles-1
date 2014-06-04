017fa405ed136ba7a8c15afb0ff2b29b5ab40649
Steven Knight <steven@knight.cx>
2014-06-04 17:53:35 -0400

    Removing as lein isn't being used.

--- lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user {:plugins [
-    [lein-marginalia "0.7.1"]
-    [lein-swank "1.4.4"]
-    [clj-ns-browser "1.3.0"]
-    [marmaduke/commons-math-3 "3"]
-    [org.clojure/data.json "0.2.0"]
-    [lein-exec "0.3.0"]
-]}}
-
