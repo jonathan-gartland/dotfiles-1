d245ff7bd3fa66eadba50c2bdbb36bdaf1ff0cce
Steven Knight <steven@knight.cx>
2014-06-18 11:47:46 -0400

    Added plugins; removed some plugins.
    Load LS_COLORS and zsh-syntax-highlighting.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -12,17 +12,23 @@ COMPLETION_WAITING_DOTS="true"
 
 plugins=(autojump archlinux
          bundler
-         colored-man colorize command-aliases command-not-found copydir copyfile cp
-         command-not-found
-         git git-prompt git-extras git-flow git-hubflow git-remote-branch github gnu-utils
+         colored-man colorize command-aliases command-not-found copydir copyfile cp command-not-found
+         dirhistory
+         gpg-agent gem git git-prompt git-extras git-flow git-hubflow git-remote-branch github gnu-utils
          history history-substring-search
          python
          rails rsync ruby rvm
-         ssh-agent sublime
-         themes thor tmux
+         ssh-agent sudo sublime systemd
+         themes tmux
+         web-search
          z)
 
 source $ZSH/oh-my-zsh.sh
+
+# https://github.com/zsh-users/zsh-syntax-highlighting
+source $HOME/dot-files-forest/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor root)
+
 source $HOME/dot-files-forest/zsh/skk.zsh-theme
 
 node_prompt_info() {
