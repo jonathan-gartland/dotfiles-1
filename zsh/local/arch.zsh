98a22b177027e9c6b757d18ce8e4946c82a93250
Steven Knight <steven@knight.cx>
2014-07-21 20:19:28 -0400

    Added ~/.cabal/bin

--- zsh/local/arch.zsh
+++ zsh/local/arch.zsh
@@ -1,2 +1,12 @@
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
+
+alias kill_subl3="pkill subl3; pkill plugin-host"
+
+# add ~/.cabal/bin if available
+[ -d "$HOME/.cabal/bin" ] && export PATH=$PATH:"$HOME/.cabal/bin"
+
+# add /usr/bin/core_perl if available
+[ -d "/usr/bin/core_perl" ] && export PATH=$PATH:"/usr/bin/core_perl"
+
+export BROWSER=google-chrome-stable
