0b608073692097416145e68c545d54227342d734
Steven Knight <steven@knight.cx>
2014-07-30 16:19:26 -0400

    Added/removed maildirs

--- bin/cleanup-maildir-HOME.sh
+++ bin/cleanup-maildir-HOME.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Arch Dev Public"
 $HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Arch General"
-$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Eclim User"
-$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Eclim Dev"
 $HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Fish User"
 $HOME/bin/cleanup-maildir --age=60 delete "Mailing Lists.i3"
-$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Ledger"
-$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Mutt User"
+$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Ruby On Rails"
 $HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Ruby Talk"
+$HOME/bin/cleanup-maildir --age=30 delete "Mailing Lists.Rust Dev"
+$HOME/bin/cleanup-maildir --age=30 delete "Trash"
+
+$HOME/bin/cleanup-maildir --age=7 delete "knightcx"
+$HOME/bin/cleanup-maildir --age=7 delete "Junk"
+$HOME/bin/cleanup-maildir --age=7 delete "github/fish-shell"
+$HOME/bin/cleanup-maildir --age=7 delete "github/oh-my-fish"
