311dc75c56aa98eb1361a9aa31103901f3da8b04
Steven Knight <steven@knight.cx>
2014-09-01 23:19:10 -0400

    Switched to phi-rectangle.

--- emacs/lib/setup_rectmark.el
+++ emacs/lib/setup_rectmark.el
@@ -1,16 +1,16 @@
-(use-package rect-mark
+(use-package phi-rectangle
   :init
   (progn
-    (bind-key "C-x r C-SPC" 'rm-set-mark)
-    (bind-key "C-x r C-x" 'rm-exchange-point-and-mark)
-    (bind-key "C-x r C-w" 'rm-kill-region)
-    (bind-key "C-x r M-w" 'rm-kill-ring-save)
+    (bind-key "C-x r C-SPC" 'phi-rectangle-set-mark)
+    (bind-key "C-x r C-x" 'phi-rectangle-exchange-point-and-mark)
+    (bind-key "C-x r C-w" 'phi-rectangle-kill-region)
+    (bind-key "C-x r M-w" 'phi-rectangle-kill-ring-save)
 
-    (autoload 'rm-set-mark "rect-mark"
+    (autoload 'phi-rectangle-set-mark "rect-mark"
       "Set mark for rectangle." t)
-    (autoload 'rm-exchange-point-and-mark "rect-mark"
+    (autoload 'phi-rectangle-exchange-point-and-mark "rect-mark"
       "Exchange point and mark for rectangle." t)
-    (autoload 'rm-kill-region "rect-mark"
+    (autoload 'phi-rectangle-kill-region "rect-mark"
       "Kill a rectangular region and save it in the kill ring." t)
-    (autoload 'rm-kill-ring-save "rect-mark"
+    (autoload 'phi-rectangle-kill-ring-save "rect-mark"
       "Copy a rectangular region to the kill ring." t)))
