a361579dd9c6eda42325d20d22791e33141f4e05
Steven Knight <steven@knight.cx>
2014-09-01 23:19:31 -0400

    Added ample-zen-theme to safe-themes list.

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(auto-insert-mode t)
  '(auto-revert-interval 1)
  '(background-color "#fdf6e3")
@@ -13,10 +14,17 @@
  '(bookmark-save-flag t)
  '(browse-url-browser-function (quote browse-url-generic))
  '(browse-url-generic-program "/opt/google/chrome/google-chrome")
- '(c-default-style (quote ((c-mode . "k&r") (c++-mode . "k&r") (java-mode . "java") (awk-mode . "awk"))))
+ '(c-default-style
+   (quote
+    ((c-mode . "k&r")
+     (c++-mode . "k&r")
+     (java-mode . "java")
+     (awk-mode . "awk"))))
  '(column-number-mode t)
  '(cursor-color "#657b83")
- '(custom-safe-themes (quote ("3341f6db5ac17e4174f7488c40676e7f0464f1e88519a59303dc7e7774245bbf" "c822d91ff777d685f67af11ec1e4b1f70ac0493e177310a0d7d93880b0c1f0b3" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "8c5ffc9848db0f9ad4e296fa3cba7f6ea3b0e4e00e8981a59592c99d21f99471" "f38dd27d6462c0dac285aa95ae28aeb7df7e545f8930688c18960aeaf4e807ed" "1760322f987b57884e38f4076ac586c27566a1d7ed421b67843c8c98a1501e3a" default)))
+ '(custom-safe-themes
+   (quote
+    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "3341f6db5ac17e4174f7488c40676e7f0464f1e88519a59303dc7e7774245bbf" "c822d91ff777d685f67af11ec1e4b1f70ac0493e177310a0d7d93880b0c1f0b3" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "8c5ffc9848db0f9ad4e296fa3cba7f6ea3b0e4e00e8981a59592c99d21f99471" "f38dd27d6462c0dac285aa95ae28aeb7df7e545f8930688c18960aeaf4e807ed" "1760322f987b57884e38f4076ac586c27566a1d7ed421b67843c8c98a1501e3a" default)))
  '(desktop-path (quote ("~/.emacs.d/.cache/")))
  '(display-time-mode t)
  '(emeteo-timeout 10)
@@ -31,7 +39,16 @@
  '(foreground-color "#657b83")
  '(fringe-mode 4 nil (fringe))
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
  '(icicle-mode t)
  '(inhibit-startup-screen t)
  '(ispell-personal-dictionary "~/.emacs.d/aspell.dict")
