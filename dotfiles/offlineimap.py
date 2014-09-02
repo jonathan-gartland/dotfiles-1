d098976f4d63aa66bb1dc032a293748d95cdb84a
Steven Knight <steven@knight.cx>
2014-09-02 19:09:49 -0400

    Update mutt to use built-in smtp. Corrected issues with offlineimap obtaining password. Updated password in msmtp.authinfo.HOME.gpg

--- dotfiles/offlineimap.py
+++ dotfiles/offlineimap.py
@@ -2,8 +2,13 @@
 
 import re, os
 
-def get_authinfo_password(machine, login, port,type):
-    s = "machine %s login %s password ([^ ]*) port %s" % (machine, login, port)
-    p = re.compile(s)
-    authinfo = os.popen("gpg -q -d ~/.authinfo.%s" % type).read()
-    return p.search(authinfo).group(1)
+# Format of ~/.msmtp.authinfo.X.gpg file
+# host: $HOST username: $USER password: $PASSWORD port: $PORT
+
+def get_authinfo_password(machine, login, port, type):
+    cmd = "gpg --no-tty --batch -d -q ~/.msmtp.authinfo.%s.gpg 2> /dev/null" % type
+    authinfo = os.popen(cmd).read()
+    return re.search("host: %s username: %s password: ([^ ]*) port: %s" % (machine, login, port), authinfo).group(1)
+
+if __name__ == '__main__':
+    print("password:", get_authinfo_password("knight.cx", "steven", "25", "home"))
