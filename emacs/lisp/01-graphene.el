7964f89f2a6ec6dce2e2b649ce7c7cf557306e9c
Steven Knight <steven@knight.cx>
2014-09-03 19:46:25 -0400

    Don't show speedbar

--- emacs/lisp/01-graphene.el
+++ emacs/lisp/01-graphene.el
@@ -1,7 +1,7 @@
 (use-package graphene
   :init 
   (progn
-    (setq graphene-speedbar-auto 0
+    (setq graphene-speedbar-auto nil
           graphene-geometry-file user-graphene-geometry-file
           projectile-cache-file user-projectile-cache-file
           projectile-known-projects-file user-projectile-known-projects-file)
