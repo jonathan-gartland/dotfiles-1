686573fb171be4c07b3917721b8982c4dcf01458
Steven Knight <steven@knight.cx>
2014-09-05 13:08:05 -0400

    changed to use-package

--- emacs/lisp/20-py.el
+++ emacs/lisp/20-py.el
@@ -1,9 +1,23 @@
-(elpy-enable)
+;; (elpy-enable)
 
-(require 'jedi)
+;; (require 'jedi)
 
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-(setq jedi:setup_keys t)
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:complete-on-dot t)
+;; (setq jedi:setup_keys t)
+
+
+(use-package ely
+  :init
+  (progn
+    (elpy-enable)))
+
+(use-package jedi
+:init
+(progn
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (setq jedi:complete-on-dot t)
+  (setq jedi:setup_keys t)))
+
 
 (provide 'setup_python)
