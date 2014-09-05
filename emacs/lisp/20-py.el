b5e9757b6c90c0f6142825a95d2f1e5d5f804d12
Steven Knight <steven@knight.cx>
2014-09-05 14:53:12 -0400

    Fixed typo.  Added key-binding.

--- emacs/lisp/20-py.el
+++ emacs/lisp/20-py.el
@@ -6,8 +6,7 @@
 ;; (setq jedi:complete-on-dot t)
 ;; (setq jedi:setup_keys t)
 
-
-(use-package ely
+(use-package elpy
   :init
   (progn
     (elpy-enable)))
@@ -15,9 +14,14 @@
 (use-package jedi
 :init
 (progn
-  (add-hook 'python-mode-hook 'jedi:setup)
-  (setq jedi:complete-on-dot t)
-  (setq jedi:setup_keys t)))
+    (bind-key "C-c /" 'jedi:get-in-function-call)
+    ;; Don't let tooltip show up automatically
+    (setq jedi:get-in-function-call-delay 10000000)
+    ;; Start completion at method dot
+    (setq jedi:complete-on-dot t)
+    (setq jedi:setup_keys t)
+
+    (add-hook 'python-mode-hook 'jedi:setup)))
 
 
 (provide 'setup_python)
