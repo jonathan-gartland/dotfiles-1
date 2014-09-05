83a789ea2bc613b01b276f386eda464e0ebcc3b1
Steven Knight <steven@knight.cx>
2014-09-05 13:09:06 -0400

    changed to use-package

--- emacs/lisp/05-autocomplete.el
+++ emacs/lisp/05-autocomplete.el
@@ -1,8 +1,35 @@
 ;;; Load yasnippet first to avoid precedence issues
-(require 'yasnippet)
-(yas-global-mode 1)
+;(require 'yasnippet)
+;(yas-global-mode 1)
 
-(require 'auto-complete-config)
+(use-package yasnippet
+  :init
+  (progn
+    (yas-global-mode 1)))
+
+;(require 'auto-complete-config)
+(use-package auto-complete-config
+:init
+(progn
+
+(setq ac-comphist-file emacs-acomphistfile-file)
+(add-to-list 'ac-dictionary-directories emacs-autocompletedict)
+
+(ac-config-default)
+(setq ac-auto-start nil)
+(setq ac-quick-help-delay 0.5)
+(ac-set-trigger-key "TAB")
+(define-key ac-mode-map  [(control tab)] 'auto-complete)
+(setq-default ac-sources '(ac-source-yasnippet
+                           ac-source-abbrev
+                           ac-source-dictionary
+                           ac-source-words-in-same-mode-buffers))
+(add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+(add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+(add-hook 'css-mode-hook 'ac-css-mode-setup)
+(add-hook 'auto-complete-mode-hook 'ac-common-setup)
+(global-auto-complete-mode t)))
 
 (setq ac-comphist-file emacs-acomphistfile-file)
 (add-to-list 'ac-dictionary-directories emacs-autocompletedict)
