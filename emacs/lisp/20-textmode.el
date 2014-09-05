120f291c7bfc396d0cdfa0ea7d0f0f3ea81107b3
Steven Knight <steven@knight.cx>
2014-09-05 14:52:11 -0400

    Fixed bug with filladapt not loading

--- emacs/lisp/20-textmode.el
+++ emacs/lisp/20-textmode.el
@@ -1,11 +1,9 @@
-(use-package textmate)
-
 (add-hook 'text-mode-hook
           (lambda ()
             (interactive)
-            (set-fill-column 78)                    ; lines are 78 chars long ...         
-            (auto-fill-mode t)                      ; ... and wrapped around automagically
-            ; (set-input-method "latin-1-prefix")     ; make " + e => ë etc.
-            ; do the intelligent wrapping of lines
-            (use-package filladapt
-              :init (filladapt-mode t))))
+            (set-fill-column 78)   ; lines are 78 chars long ...         
+            (auto-fill-mode t)     ; ... and wrapped around automatically
+            ))
+
+(use-package filladapt
+  :init (filladapt-mode t))
