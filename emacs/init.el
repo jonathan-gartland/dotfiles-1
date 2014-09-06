68c96ebc85b4d2367080979c9a0ec5f89a84ca48
Steven Knight <steven@knight.cx>
2014-09-06 14:51:44 -0400

    Changed how defuns are loaded.

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,3 @@
-; No splash screen please ... jeez
-(setq inhibit-startup-message t)
 (setq is-mac (equal system-type 'darwin))
 (setq is-linux (equal system-type 'gnu/linux))
 (setq is-windows-nt (equal system-type 'windows-nt))
@@ -10,17 +8,12 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+; https://github.com/ajtulloch/dots/tree/master/emacs
+
 ; common lisp goodies, loop
 (require 'cl)
 
-;; Functions (load all files in defuns-dir)
-;(setq defuns-dir (expand-file-name "lisp/defuns" user-emacs-directory))
-;(dolist (file (directory-files defuns-dir t "\\w+"))
-;  (when (file-regular-p file)
-;    (load file)))
-
 (require 'use-package)
-;(setq use-package-verbose t)
 
 ; Load setup/init files
 (mapc 'load (directory-files (expand-file-name "lisp" user-emacs-directory) t "^[0-9]+.*\.el$"))
@@ -33,6 +26,6 @@
 
 (setq custom-file (expand-file-name "lisp/99-custom.el" user-emacs-directory))
 
-(add-hook 'after-init-hook (lambda ()
-			     (setq initial-scratch-message
-                 (message "Startup time: %s" (emacs-init-time)))))
+;; (add-hook 'after-init-hook (lambda ()
+;; 			     (setq initial-scratch-message
+;;                  (message "Startup time: %s" (emacs-init-time)))))
