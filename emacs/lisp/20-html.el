85af0ff3558b348f722d4176724d3007fb1677c6
Steven Knight <steven@knight.cx>
2014-09-06 14:30:38 -0400

    Switched to web-mode.

--- emacs/lisp/20-html.el
+++ emacs/lisp/20-html.el
@@ -1,6 +1,16 @@
-(add-to-list 'auto-mode-alist '("\\.html\\'" . nxml-mode))
-
- (setq nxml-child-indent 4
-       nxml-outline-child-indent 2)
-
-
+; http://web-mode.org/
+(use-package web-mode
+             :init
+             (progn
+               (setq web-mode-ac-sources-alist
+                     '(("css" . (ac-source-css-property))
+                       ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+                     
+               (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+               (add-to-list 'auto-mode-alist '("\\.css" . web-mode))
+               (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+               
+               ; https://github.com/smihica/emmet-mode
+               (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+               (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+               ))
