f7fae5eba14e888ff7a313c9e272bddbecd19735
Steven Knight <steven@knight.cx>
2014-09-06 14:28:55 -0400

    Added various git related modules.

--- emacs/lisp/10-git.el
+++ emacs/lisp/10-git.el
@@ -1,6 +1,7 @@
+; https://github.com/magit/git-modes
 
-;(use-package git-emacs)
-;(use-package git-modeline)
-
-(use-package gitignore-mode)
+(use-package git-commit-mode)
+(use-package git-rebase-mode)
 (use-package gitconfig-mode)
+(use-package gitignore-mode)
+(use-package gitattributes-mode)
