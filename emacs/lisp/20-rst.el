483bc03425c27ef7f17728527dff29c874c5eac5
Steven Knight <steven@knight.cx>
2014-09-06 14:44:15 -0400

    Enable rst-mode for reStructured files.

--- emacs/lisp/20-rst.el
+++ emacs/lisp/20-rst.el
@@ -1,2 +1,6 @@
-; http://docutils.sourceforge.net/docs/user/emacs.html
-(use-package rst)
+(use-package rst
+  :init
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.txt$" . rst-mode))
+    (add-to-list 'auto-mode-alist '("\\.rst$" . rst-mode))
+    (add-to-list 'auto-mode-alist '("\\.rest$" . rst-mode))))
