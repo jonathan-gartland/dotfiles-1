1401b158ab973998690d66e6d8bbc2525fe422e4
Steven Knight <steven@knight.cx>
2014-09-06 14:52:45 -0400

    Enable ruby-mode for various ruby files.

---
+++ emacs/lisp/20-ruby.el
@@ -0,0 +1,11 @@
+;; Ruby
+(autoload 'rhtml-mode "rhtml-mode")
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.watchr$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
+
