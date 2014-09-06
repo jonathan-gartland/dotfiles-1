3d509494bcf2e8628ca74cd9af85a8794106623c
Steven Knight <steven@knight.cx>
2014-09-06 14:43:11 -0400

    Added comment about traad.  Removed commented section.

--- emacs/lisp/20-py.el
+++ emacs/lisp/20-py.el
@@ -1,13 +1,5 @@
-;; (elpy-enable)
-
-;; (require 'jedi)
-
-;; (add-hook 'python-mode-hook 'jedi:setup)
-;; (setq jedi:complete-on-dot t)
-;; (setq jedi:setup_keys t)
-
 ;; TODO check out traad https://github.com/abingham/traad
-;; Traad is a client-server approach to using the rope Python refactory library.
+;; Traad is a client-server approach to use the rope Python refactory library.
 
 (use-package elpy
   :init
@@ -15,13 +7,13 @@
     (elpy-enable)))
 
 (use-package jedi
-:init
-(progn
+  :init
+  (progn
     (bind-key "C-c /" 'jedi:get-in-function-call)
     ;; Don't let tooltip show up automatically
     (setq jedi:get-in-function-call-delay 10000000)
     ;; Start completion at method dot
     (setq jedi:complete-on-dot t)
     (setq jedi:setup_keys t)
-
+    (add-hook 'python-mode-hook 'eldoc-mode)
     (add-hook 'python-mode-hook 'jedi:setup)))
