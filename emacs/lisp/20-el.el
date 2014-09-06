75456b17797f93f8a30d09292f84ec599b2e06bb
Steven Knight <steven@knight.cx>
2014-09-06 14:52:20 -0400

    Enable emacs-lisp-mode for Cask and Carton.  Added hooks.

---
+++ emacs/lisp/20-el.el
@@ -0,0 +1,8 @@
+;; elisp
+(add-to-list 'auto-mode-alist '("Carton$" . emacs-lisp-mode))
+(add-to-list 'auto-mode-alist '("Cask$" . emacs-lisp-mode))
+
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'eldoc-mode)
+(add-hook 'ielm-mode-hook 'eldoc-mode)
+
