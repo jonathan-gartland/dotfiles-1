3e28d6d72f59c6bccba6637df2f77f3c68ba557b
Steven Knight <steven@knight.cx>
2014-09-06 14:42:14 -0400

    Removed setting ido variables as graphene does it.

--- emacs/lisp/60-ido.el
+++ emacs/lisp/60-ido.el
@@ -1,19 +1,6 @@
 (use-package flx-ido
   :init
   (progn
-    (bind-key "C-x C-i" 'ido-imenu)
-    (ido-mode 1)
-    (ido-everywhere 1)
+    ;(bind-key "C-x C-i" 'ido-imenu)
     (flx-ido-mode 1)
-    (setq ido-create-new-buffer (quote never))
-    (setq ido-enable-flex-matching t)
-    (setq ido-enable-last-directory-history nil)
-    (setq ido-enable-regexp nil)
-    (setq ido-max-directory-size 300000)
-    (setq ido-max-file-prompt-width 0.1)
-    (setq ido-use-filename-at-point (quote guess))
-    (setq ido-use-url-at-point t)
-    (setq ido-use-virtual-buffers t)
-    ;; enable fuzzy matching
-    (setq ido-enable-flex-matching t)
-    ))
+    (setq ido-use-faces nil)))
