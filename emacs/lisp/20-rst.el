3e697f1e0d253f2663f56297db708fd870aff263
Steven Knight <steven@knight.cx>
2014-09-06 14:44:48 -0400

    Don't enable for txt file.

--- emacs/lisp/20-rst.el
+++ emacs/lisp/20-rst.el
@@ -1,6 +1,5 @@
 (use-package rst
   :init
   (progn
-    (add-to-list 'auto-mode-alist '("\\.txt$" . rst-mode))
     (add-to-list 'auto-mode-alist '("\\.rst$" . rst-mode))
     (add-to-list 'auto-mode-alist '("\\.rest$" . rst-mode))))
