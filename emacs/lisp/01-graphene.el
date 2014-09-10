6caed70554ae82b49af42a6a7af7ace1f2f05e41
Steven Knight <steven@knight.cx>
2014-09-10 12:59:36 -0400

    Switched to use eval-after-load to reduce emacs-init-time.

--- emacs/lisp/01-graphene.el
+++ emacs/lisp/01-graphene.el
@@ -1,8 +1,22 @@
-(use-package graphene
-  :init 
-  (progn
-    (setq graphene-speedbar-auto nil
-          graphene-geometry-file user-graphene-geometry-file
-          projectile-cache-file user-projectile-cache-file
-          projectile-known-projects-file user-projectile-known-projects-file)
-    (projectile-global-mode)))
+
+;; (use-package graphene
+;;   :init 
+;;   (progn
+;;     (setq graphene-speedbar-auto nil
+;;           graphene-geometry-file user-graphene-geometry-file
+;;           projectile-cache-file user-projectile-cache-file
+;;           projectile-known-projects-file user-projectile-known-projects-file)
+;;     (projectile-global-mode)))
+;; ;
+
+(add-hook 'after-init-hook 
+          (lambda ()
+            (use-package graphene
+              :init 
+              (progn
+                (setq graphene-speedbar-auto nil
+                      graphene-geometry-file user-graphene-geometry-file
+                      projectile-cache-file user-projectile-cache-file
+                      projectile-known-projects-file user-projectile-known-projects-file)
+                (projectile-global-mode)))))
+
