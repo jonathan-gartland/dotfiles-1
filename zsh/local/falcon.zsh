044e60f67934bc5f51e54e989cf4a9fd239a33e8
Steven Knight <steven@knight.cx>
2014-09-10 13:07:11 -0400

    Use path, rather than $PATH.

--- zsh/local/falcon.zsh
+++ zsh/local/falcon.zsh
@@ -2,10 +2,10 @@ export PRINTER=HP_LaserJet_1020
 alias reset_home_printer="sudo sed -i -e ‘/StateMessage .*lpd failed/d’ -e ‘s/State Stopped/State Idle/’ /etc/cups/printers.conf"
 alias kill_subl3="pkill subl3; pkill plugin-host"
 
-# add ~/.cabal/bin if available
-[ -d "$HOME/.cabal/bin" ] && export PATH=$PATH:"$HOME/.cabal/bin"
-
 # add /usr/bin/core_perl if available
-[ -d "/usr/bin/core_perl" ] && export PATH=$PATH:"/usr/bin/core_perl"
+[ -d "/usr/bin/core_perl" ] && path=(/usr/bin/core_perl $path)
+
+# add ~/.cabal/bin if available
+[ -d "$HOME/.cabal/bin" ] && path=($HOME/.cabal/bin $path)
 
 export BROWSER=google-chrome-stable
