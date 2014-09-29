b005562a2120837511264f68c359b345d64c5e94
Steven Knight <steven@knight.cx>
2014-09-28 21:42:20 -0400

    Disabled diminishing of all packages.

--- emacs/lisp/50-diminish.el
+++ emacs/lisp/50-diminish.el
@@ -1,30 +1,25 @@
-;;; Diminish --- Summary
-;;; Commentary:
-;;; Code:
+;(require 'diminish)
 
-(require 'diminish)
+;(eval-after-load "flyspell"
+  ;'(diminish 'flyspell-mode))
+;(eval-after-load "whitespace"
+  ;'(diminish 'global-whitespace-mode))
+;(eval-after-load "flycheck"
+  ;'(setq-default flycheck-mode-line-lighter " ✓"))
+;(eval-after-load "smartparens"
+  ;'(diminish 'smartparens-mode))
 
-(eval-after-load "flyspell"
-  '(diminish 'flyspell-mode))
-(eval-after-load "whitespace"
-  '(diminish 'global-whitespace-mode))
-(eval-after-load "flycheck"
-  '(setq-default flycheck-mode-line-lighter " ✓"))
-(eval-after-load "smartparens"
-  '(diminish 'smartparens-mode))
-
-(eval-after-load "hi-lock"
-  '(diminish 'hi-lock-mode))
+;(eval-after-load "hi-lock"
+  ;'(diminish 'hi-lock-mode))
 
 
-(diminish 'auto-complete-mode)
-(diminish 'yas-minor-mode)
-(diminish 'magit-auto-revert-mode)
-(diminish 'auto-fill-function)
-(diminish 'isearch-mode)
+;(diminish 'auto-complete-mode)
+;(diminish 'yas-minor-mode)
+;(diminish 'magit-auto-revert-mode)
+;(diminish 'auto-fill-function)
+;(diminish 'isearch-mode)
 
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (setq mode-name "el")))
+;(add-hook 'emacs-lisp-mode-hook
+          ;(lambda ()
+            ;(setq mode-name "el")))
 
-;;; 50-diminish ends here
