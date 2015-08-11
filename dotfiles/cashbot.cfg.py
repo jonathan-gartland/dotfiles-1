0767c1b1cc5f5149dd6dcb54dda82513622b974c
Steven Knight <steven.knight@cashstar.com>
2015-08-11 10:48:53 -0400

    Added cashbox con file.

---
+++ dotfiles/cashbot.cfg.py
@@ -0,0 +1,8 @@
+{
+    'REPORT_TYPE': 'HTML',
+    'REPORT_RECIPIENTS': 'steven.knight@cashstar.com',
+    'JENKINS_USERNAME': 'sknight',
+    'JENKINS_API_TOKEN': '26ca5d6637ab45aedefdd056e9fe78c5',
+    'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False},
+    # 'CONSOLE_LOG_LEVEL': 'DEBUG',
+}
