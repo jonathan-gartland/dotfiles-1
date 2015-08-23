357e709bc85f7d2e89a7920dd65fb16472393cdc
Steven Knight <steven.knight@cashstar.com>
2015-08-22 20:50:15 -0400

    Added submodule hammerspoon-config.  Added zsh file for MBP.

--- zsh/local/MBP-NARASIMHA.local.zsh
+++ zsh/local/MBP-NARASIMHA.local.zsh
@@ -1,6 +1,7 @@
 # add /usr/texbin to PATH, if available
-[ -d "/usr/texbin" ] && path=(/usr/texbin $path)
+[[ -d "/usr/texbin" ]] && path=(/usr/texbin $path)
 
 alias QA='workon QA; cd ~/Projects/QA'
-
 alias start_selenium='cd ~/Projects/QA/webdriver/MacOSX/; sh start_webdriver.sh'
+
+alias dircolors=gdircolors
