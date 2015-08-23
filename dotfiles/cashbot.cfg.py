e71306ac4cbbccc9874dcdc6635d2c93a30e6d03
Steven Knight <steven.knight@cashstar.com>
2015-08-22 20:50:57 -0400

    Added cfg file for cashbox.

--- dotfiles/cashbot.cfg.py
+++ dotfiles/cashbot.cfg.py
@@ -4,5 +4,6 @@
     'JENKINS_USERNAME': 'sknight',
     'JENKINS_API_TOKEN': '26ca5d6637ab45aedefdd056e9fe78c5',
     'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False},
+    # 'LOG_DB_URL': 'sqlite:///tmp/cashbot.db',
     # 'CONSOLE_LOG_LEVEL': 'DEBUG',
 }
