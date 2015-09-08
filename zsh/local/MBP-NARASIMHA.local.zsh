9b281c9885a9d5cf2564399273a91222a12d4b53
Steven Knight <steven.knight@cashstar.com>
2015-09-08 19:31:03 -0400

    Added aliases, set PYTHONPATH.

--- zsh/local/MBP-NARASIMHA.local.zsh
+++ zsh/local/MBP-NARASIMHA.local.zsh
@@ -5,3 +5,8 @@ alias QA='workon QA; cd ~/Projects/QA'
 alias start_selenium='cd ~/Projects/QA/webdriver/MacOSX/; sh start_webdriver.sh'
 
 alias dircolors=gdircolors
+
+alias show_dot_files='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hide_dot_files='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+export PYTHONPATH=$HOME/src/hooks:$PYTHONPATH
