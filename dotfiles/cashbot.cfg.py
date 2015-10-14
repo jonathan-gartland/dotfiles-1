c0ab701c4fe9a6e776678740357ba2d0e0d89e99
Steven Knight <steven.knight@cashstar.com>
2015-10-14 16:55:43 -0400

    Switched to mysql database.

--- dotfiles/cashbot.cfg.py
+++ dotfiles/cashbot.cfg.py
@@ -1,9 +1,20 @@
 {
     'REPORT_TYPE': 'HTML',
     'REPORT_RECIPIENTS': 'steven.knight@cashstar.com',
+
     'JENKINS_USERNAME': 'sknight',
     'JENKINS_API_TOKEN': '26ca5d6637ab45aedefdd056e9fe78c5',
+
     'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False},
-    # 'LOG_DB_URL': 'sqlite:///tmp/cashbot.db',
-    # 'CONSOLE_LOG_LEVEL': 'DEBUG',
+
+    # 'LOG_DB_URL': 'sqlite:///Users/steven.knight/Projects/qa/cashbot.db',
+    # 'LOG_DB_URL': 'sqlite:/:memory:',
+    'LOG_DB_URL': "mysql://cashbot:jJT!3VK14&llVP0o@localhost:3306/cashbotdb",
+
+    'LOG_DB_DEBUG': False,
+    # 'LOG_DB_DEBUG': True,
+
+    'CONSOLE_LOG_LEVEL': 'DEBUG',
 }
+
+
