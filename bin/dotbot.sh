1c845f6b7d50bd1f53fb6d0854a4be1b3af23cd4
Steven Knight <steven.knight@cashstar.com>
2015-10-15 19:22:34 -0400

    Added wrapper-script for dotbot.

---
+++ bin/dotbot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~/src/dotbot
+
+virtualenv .
+. ./bin/activate
+
+pip install --upgrade pip
+pip install PyYaml
+
+base_dir=~/dot-files-forest
+config_file=~/dot-files-forest/install.conf.yaml
+
+~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $config_file
+
+deactivate
