7505b23ec6a257d819ee975c710da66f5a87d27c
Steven Knight <steven.knight@cashstar.com>
2015-10-18 13:42:48 -0400

    Added platform specific install.conf.yaml files.

--- bin/dotbot.sh
+++ bin/dotbot.sh
@@ -10,7 +10,17 @@ pip install PyYaml
 
 base_dir=~/dot-files-forest
 config_file=~/dot-files-forest/install.conf.yaml
+osx_config_file=~/dot-files-forest/osx_install.conf.yaml
+linux_config_file=~/dot-files-forest/linux_install.conf.yaml
 
 ~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $config_file -q
+
+if [[ "$OSTYPE" == darwin* ]]; then
+    ~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $osx_config_file -q
+fi
+
+if [[ "$OSTYPE" == linux* ]]; then
+    ~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $linux_config_file -q
+fi
 
 deactivate
