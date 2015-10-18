00dd47f75f2698c48ad57f1f64a06a402a981ec3
Steven Knight <steven.knight@cashstar.com>
2015-10-18 12:44:34 -0400

    Added features from bullettrain-zsh prompt

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -1,3 +1,4 @@
+
 # prompt
 setopt prompt_subst
 
@@ -7,8 +8,11 @@ precmd() {
     vcs_info
 }
 
-# mode-aware arrow
+CONTEXT_SHOW=true
 
+# derived from https://github.com/blaenk/dots/blob/master/zsh/zsh/prompt.zsh
+
+# mode-aware arrow
 function p_arrow {
   if [[ $KEYMAP = "vicmd" ]]; then
     echo "%F{magenta}»%f"
@@ -18,40 +22,62 @@ function p_arrow {
 }
 
 # colored path
-
 function p_colored_path {
   local slash="%F{cyan}/%f"
   echo "${${PWD/#$HOME/~}//\//$slash}"
 }
 
-# git info
-
+# hg/git info
 function p_vcs {
   vcs_info
   echo $vcs_info_msg_0_
 }
 
+# Context: user@hostname (who am I and where am I)
+context() {
+  local user="$(whoami)"
+  [[ "$user" != "$CONTEXT_DEFAULT_USER" || -n "$IS_SSH_CLIENT" ]] && echo -n "${user}@%m"
+}
+prompt_context() {
+  local _context="$(context)"
+  [[ -n "$_context" ]] && echo "$_context"
+}
+
+# Status:
+# - was there an error
+# - am I root
+# - are there background jobs?
+prompt_status() {
+  local symbols
+  symbols=()
+  [[ $RETVAL -ne 0 ]] && symbols+="✘"
+  [[ $RETVAL -ne 0 ]] && symbols+="✘ $RETVAL"
+  [[ $UID -eq 0 ]] && symbols+="%{%F{yellow}%}⚡%f"
+  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="⚙"
+
+  if [[ -n "$symbols" && $RETVAL -ne 0 ]]; then
+    echo "$symbols"
+  elif [[ -n "$symbols" ]]; then
+    echo "$symbols"
+  fi
+}
+
 # environments:
 #  - ssh
-#  - virtualenv
-#  - cabal sandbox
-
-export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 function p_envs {
-  # check for cabal sandbox in parent directories, recursively
-  # local cabal
-  # cabal=( (../)#cabal.sandbox.config(N) )
-
   local envs
   [[ -n $SSH_CLIENT ]]  && envs+="R"
-  # (( $#cabal ))         && envs+="H"
-  [[ -n $VIRTUAL_ENV ]] && envs+="P"
+  [[ -n $envs ]] && echo "%F{green}[%f$envs%F{green}]%f"
+}
 
-  [[ -n $envs ]] && echo " %F{green}[%f$envs%F{green}]%f"
+
+function virtualenv_prompt_info {
+  [ $VIRTUAL_ENV ] && '('`basename $VIRTUAL_ENV`') '
 }
 
 PROMPT='
-%F{blue}λ%f $(p_colored_path)$(p_envs)$(p_vcs)
+%F{blue}λ%f $(prompt_context) $(p_colored_path) $(p_envs) $(p_vcs) $(prompt_status)
 $(p_arrow) '
 
+RPROMPT='$(virtualenv_prompt_info)'
