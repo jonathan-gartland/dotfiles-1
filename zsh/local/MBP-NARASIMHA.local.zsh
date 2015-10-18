9cf3ddd3d5c6e46b0544a6fd4ddb8e10d3f918d3
Steven Knight <steven.knight@cashstar.com>
2015-10-18 13:43:02 -0400

    Removed commented out code.

--- zsh/local/MBP-NARASIMHA.local.zsh
+++ zsh/local/MBP-NARASIMHA.local.zsh
@@ -1,9 +1,6 @@
 # add /usr/texbin to PATH, if available
 [[ -d "/usr/texbin" ]] && path=(/usr/texbin $path)
 
-# alias QA='workon QA; cd ~/Projects/QA'
-# alias start_selenium='cd ~/Projects/QA/webdriver/MacOSX/; sh start_webdriver.sh'
-
 alias dircolors=gdircolors
 
 alias show_dot_files='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
