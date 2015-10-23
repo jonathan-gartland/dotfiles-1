9f94f40b45f0fda920ccfae5306a3fbce5505cb3
Steven Knight <steven.knight@cashstar.com>
2015-10-22 22:12:59 -0400

    Added link for powerline.config

--- zsh/lib/40_prompt.zsh
+++ zsh/lib/40_prompt.zsh
@@ -1,3 +1,3 @@
 
 powerline-daemon -q
-source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+source_if_exists /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
