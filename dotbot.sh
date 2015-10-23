c69600df6a94174ed299dbb0c0ea50503cb1430f
Steven Knight <steven.knight@cashstar.com>
2015-10-22 20:26:48 -0400

    Moved dotbot.sh script

--- dotbot.sh
+++ dotbot.sh
@@ -13,7 +13,7 @@ config_file=~/dot-files-forest/install.conf.yaml
 osx_config_file=~/dot-files-forest/osx_install.conf.yaml
 linux_config_file=~/dot-files-forest/linux_install.conf.yaml
 
-~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $config_file -q
+~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $config_file
 
 if [[ "$OSTYPE" == darwin* ]]; then
     ~/src/dotbot/bin/dotbot --base-directory $base_dir --config-file $osx_config_file -q
