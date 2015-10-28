11998b985aabd45f90947d0c5ac6ba3e8adbe3b6
Steven Knight <steven.knight@cashstar.com>
2015-10-28 10:11:47 -0400

    switched to using liquidprompt

--- zsh/lib/40_prompt.zsh
+++ zsh/lib/40_prompt.zsh
@@ -1,4 +1,3 @@
 
-powerline-daemon -q
-# testing: powerline-daemon --foreground --replace
-source_if_exists /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+# use liquidprompt
+source ~/dot-files-forest/liquidprompt/liquidprompt
