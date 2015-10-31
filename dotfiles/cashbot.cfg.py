fe6e531d88efbab53607182910edac68d2c9ac41
Steven Knight <steven.knight@cashstar.com>
2015-10-31 13:59:29 -0400

    Add ANYBAR support

--- dotfiles/cashbot.cfg.py
+++ dotfiles/cashbot.cfg.py
@@ -5,11 +5,13 @@
     'JENKINS_USERNAME': 'sknight',
     'JENKINS_API_TOKEN': '594849a68d4911d6c39a2cb5f700c385',
 
-    'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False},
+    'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False}, # , 'ANYBAR': True},
 
     # 'LOG_DB_URL': 'sqlite:///Users/steven.knight/Projects/qa/cashbot.db',
     # 'LOG_DB_URL': 'sqlite:/:memory:',
     'LOG_DB_URL': "mysql://cashbot:jJT!3VK14&llVP0o@localhost:3306/cashbotdb",
+
+    # 'ANYBAR_COLOR_FAIL': 'blue',
 
     # 'GNUPG_VERBOSE': True,
     # 'LOG_DB_DEBUG': True,
