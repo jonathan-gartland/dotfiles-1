765097839de71e6c635089c814390219f5bb0203
Steven Knight <steven@knight.cx>
2015-10-31 18:14:32 -0400

    Moved antigen loading of virtualenvwrapper

--- zsh/lib/10_python.zsh
+++ zsh/lib/10_python.zsh
@@ -9,12 +9,14 @@ export PIP_RESPECT_VIRTUALENV=true
 export VIRTUALENV_USE_DISTRIBUTE=True
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS=--no-site-packages
 
-
 if [[ "$OS_IS_POSIX" = true ]]; then
     # create virtualenv py if necessary
-    lsvirtualenv -b |grep py
+    lsvirtualenv -b |grep py > /dev/null 2>&1
 
     if [ $? -eq 1 ]; then
         mkvirtualenv py
     fi
+
+    antigen bundle virtualenvwrapper
 fi
+
