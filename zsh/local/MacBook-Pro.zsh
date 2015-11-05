b84b3edd061f7e946c4404a555ba7544167a9a0d
Steven Knight <steven.knight@cashstar.com>
2015-11-05 11:36:59 -0500

    Add zsh local file to MBP

--- zsh/local/MacBook-Pro.zsh
+++ zsh/local/MacBook-Pro.zsh
@@ -10,3 +10,7 @@ export PYTHONPATH=$HOME/src/hooks:$PYTHONPATH
 
 # https://github.com/tonsky/AnyBar
 function anybar { echo -n $1 | nc -4u -w0 localhost ${2:-1738}; }
+
+antigen bundle vagrant
+
+antigen apply
