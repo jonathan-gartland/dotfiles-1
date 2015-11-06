8ba5c451149e82cbc510a1632603a7f28ca8e4f5
Steven Knight <steven.knight@cashstar.com>
2015-11-06 13:14:22 -0500

    enable ANYBAR

--- dotfiles/cashbot.cfg.py
+++ dotfiles/cashbot.cfg.py
@@ -5,7 +5,7 @@
     'JENKINS_USERNAME': 'sknight',
     'JENKINS_API_TOKEN': '594849a68d4911d6c39a2cb5f700c385',
 
-    'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False}, # , 'ANYBAR': True},
+    'FEATURES': {'PASSWORD_DECRYPTION': False, 'AWS': False, 'ANYBAR': True},
 
     # 'LOG_DB_URL': 'sqlite:///Users/steven.knight/Projects/qa/cashbot.db',
     # 'LOG_DB_URL': 'sqlite:/:memory:',
