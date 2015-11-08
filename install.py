f3a6ff43920ae253111f11c40c1a723d63164b2d
Steven Knight <steven.knight@cashstar.com>
2015-11-08 10:00:32 -0500

    Add support for cloning submodules to install.py script

--- install.py
+++ install.py
@@ -16,9 +16,14 @@ def get_parser(arguments):
     usage = "usage: %prog [options] home|work|none"
     parser = optparse.OptionParser(usage = usage)
 
+    parser.add_option('-b', '--bootstrap', dest='bootstrap', default=False,
+                      help='Download all required repos', action='store_true')
+    parser.add_option('-l', '--links', dest='links', default=False,
+                      help='Create all links', action='store_true')
+
     parser.add_option("-v", "--verbose", dest="verbose", default=False,
-        help="Print lots of debugging information",
-        action="store_true")
+                      help="Print lots of debugging information",
+                      action="store_true")
     parser.add_option("-n", "--dry-run", dest="dry_run", default=False,
                       help="Don't actually run any commands; just print them.",
                       action="store_true")
@@ -33,36 +38,89 @@ def run(argv):
         parser.print_help()
         return 1
 
-    if args[1].upper() == "WORK":     install_type = install.WORK
-    elif args[1].upper() == "HOME":   install_type = install.HOME
-    else:                             install_type = None
-
-    platform_system = platform.system()
-
-    if platform_system.startswith('Darwin'):
-        os_type = install.OSX
-    elif platform_system.startswith('Linux'):
-        os_type = install.LINUX
-    elif platform_system.startswith('Windows'):
-        os_type = install.WINDOWS
+    if options.links:
+       links(argv, options).run()
+    elif options.bootstrap:
+        bootstrap(argv, options).run()
     else:
-        os_type = None
-
-    install(install_type, os_type, options).run()
+        raise ValueError("Need either option links or bootstrap")
 
     return 0
 
-class install(object):
+class InstallBase(object):
 
     WORK = 1
     HOME = 2
-    OSX = 3
-    LINUX = 4
-    WINDOWS = 5
+    NONE = 3
 
-    def __init__(self, install_type, os_type, options):
-        self.install_type = install_type
-        self.os_type = os_type
+    OSX = 4
+    LINUX = 5
+    WINDOWS = 6
+
+    def __init__(self, argv, options):
+        self.argv = argv
+        self.options = options
+
+        # TODO: convert to dict syntax
+        if self.argv[1].upper() == "WORK":
+            self.install_type = InstallBase.WORK
+        elif self.argv[1].upper() == "HOME":
+            self.install_type = InstallBase.HOME
+        else:
+            self.install_type = InstallBase.NONE
+
+        # TODO: convert to dict syntax
+        platform_system = platform.system()
+        if platform_system.startswith('Darwin'):
+            self.os_type = InstallBase.OSX
+        elif platform_system.startswith('Linux'):
+            self.os_type = InstallBase.LINUX
+        elif platform_system.startswith('Windows'):
+            self.os_type = InstallBase.WINDOWS
+        else:
+            self.os_type = None
+
+    def run(self):
+        raise NotImplementedError
+
+    def _execute_command(self, command):
+        try:
+            retcode = call(command, shell=True)
+            if retcode < 0:
+                print >>sys.stderr, "Child was terminated by signal", -retcode
+
+        except OSError, e:
+                print >>sys.stderr, "Execution failed:", e
+
+class bootstrap(InstallBase):
+
+    def __init__(self, argv, options):
+        super(bootstrap, self).__init__(argv, options)
+
+        self.repos = [
+            'git@github.com:trapd00r/LS_COLORS.git',
+	        'git@github.com:zsh-users/antigen.git',
+	        'git@github.com:tmux-plugins/tpm.git',
+	        'git@github.com:Linell/hammerspoon-config.git',
+	        'git@github.com:skk/dotbot.git',
+	        'git@github.com:syl20bnr/spacemacs.git',
+	        'git@github.com:cxreg/smartcd.git',
+	        'git@github.com:nojhan/liquidprompt.git',
+	        'git@github.com:amix/vimrc.git',
+        ]
+
+    def clone_git_repo(self, repo):
+        self._execute_command("git clone {repo} --recurse-submodules".
+                              format(repo=repo))
+    def run(self):
+        for repo in self.repos:
+            self.clone_git_repo(repo)
+
+class links(InstallBase):
+
+    def __init__(self, argv, options):
+        super(links, self).__init__(argv, options)
+
         self.base_dir = os.path.join(os.environ['HOME'], 'dot-files-forest')
         self.config_files = {
             install.WORK: "work",
@@ -74,8 +132,6 @@ class install(object):
         }
         self.config_file = "install.conf.yaml"
         self.dotbot = "~/src/dotbot/bin/dotbot"
-        self.options = options
-
         self.verbose = self.quiet = ''
         if options.verbose:
             self.verbose = '-v'
@@ -111,16 +167,6 @@ class install(object):
             print "command: %s" % cmd
             if not self.dry_run:
                 self._execute_command(cmd)
-
-    def _execute_command(self, command):
-        try:
-            retcode = call(command, shell=True)
-            if retcode < 0:
-                print >>sys.stderr, "Child was terminated by signal", -retcode
-
-        except OSError, e:
-                print >>sys.stderr, "Execution failed:", e
-
 
 if __name__ == '__main__':
     sys.exit(run(sys.argv))
