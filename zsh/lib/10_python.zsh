3a900ce97d46c1e4c6812ca770ed7d770063e207
Steven Knight <steven.knight@cashstar.com>
2016-01-06 12:40:00 -0500

    Set PROJECT_ROOT.

--- zsh/lib/10_python.zsh
+++ zsh/lib/10_python.zsh
@@ -3,6 +3,7 @@ export PYTHONSTARTUP=$HOME/dot-files-forest/dotfiles/pythonstartup.py
 
 # See http://www.doughellmann.com/docs/virtualenvwrapper/
 export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_REQUIRE_VIRTUALENV=true
 export PIP_RESPECT_VIRTUALENV=true
