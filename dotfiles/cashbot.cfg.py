68215c7e5790220c9a956940ef1790c8880f2b2e
Steven Knight <steven.knight@cashstar.com>
2016-04-04 17:02:39 -0400

    Add gpg and imagemagick binaries

--- dotfiles/cashbot.cfg.py
+++ dotfiles/cashbot.cfg.py
@@ -3,20 +3,23 @@
 
     'JENKINS_USERNAME': 'sknight',
     'JENKINS_API_TOKEN': '594849a68d4911d6c39a2cb5f700c385',
-
+    'GNUPG_BINARY': '/usr/local/bin/gpg',
+    # 'GNUPG_VERBOSE': True,
     'FEATURES': {
         'PASSWORD_DECRYPTION': False,
         'AWS': False,
-        'ANYBAR': True
+        'ANYBAR': True,
     },
 
+    # 'LOG_DB_DEBUG': True,
     # 'LOG_DB_URL': 'sqlite:///Users/steven.knight/Projects/qa/cashbot.db',
     # 'LOG_DB_URL': 'sqlite:/:memory:',
     'LOG_DB_URL': "mysql://cashbot:jJT!3VK14&llVP0o@localhost:3306/cashbotdb",
     'LOG_DB_CONSOLE_FORMAT_STRING': '%(relativeCreated)-8d %(levelname)s %(filename)s:%(lineno)d %(module)s.%(funcName)s %(message)s',
 
+    'IMAGEMAGICK_CONVERT_BINARY': '/usr/local/bin/convert',
+    'IMAGEMAGICK_COMPARE_BINARY': '/usr/local/bin/compare',
+
     'PRINT_TRACEBACK': True,
-    # 'GNUPG_VERBOSE': True,
-    # 'LOG_DB_DEBUG': True,
     # 'CONSOLE_LOG_LEVEL': 'DEBUG',
 }
