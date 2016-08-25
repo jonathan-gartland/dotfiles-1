60722916c5c2f2d287d3edfca813bd01772ebda1
Steven Knight <steven.knight@cashstar.com>
2016-08-25 09:39:43 -0400

    Remove commented out code.

--- zsh/zlogin.zsh
+++ zsh/zlogin.zsh
@@ -1,8 +1,3 @@
-# echo "zlogin - top"
-
 # Load RVM into a shell session *as a function*
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-# [ -r "$HOME/.smartcd_config" ] && ( [ -n $BASH_VERSION ] || [ -n $ZSH_VERSION ] ) && source ~/.smartcd_config
-
-# echo "zlogin - btm"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
