743c5d3d901844eb7444e351b62dcb84d723d1e0
Steven Knight <steven.knight@cashstar.com>
2016-08-25 09:38:50 -0400

    Remove commented out code.  Remove source of files in zshenv.zsh. 

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,38 +1,8 @@
-#
-# Defines environment variables.
-#
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
+# echo "zshenv - top"
 
 # Ensure that a non-login, non-interactive shell has a defined environment.
 if [[ "$SHLVL" -eq 1 && ! -o LOGIN && -s "${ZDOTDIR:-$HOME}/.zprofile" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprofile"
 fi
 
-# load antigen
-source ~/src/antigen/antigen.zsh
-
-source "$HOME/.zsh/lib/00_base.zsh"
-
-# strict control over source order
-sources=(
-    # '03_functions'
-    # '04_path'
-    '10_python'
-    '11_ruby'
-    # '20_mysql'
-    # '30_linux'
-    # '31_osx'
-    # '40_prompt'
-    # '50_hub'
-    # '51_vcsinfo'
-    # '60_zle'
-    # '61_highlight'
-    # '62_misc'
-    # '63_smartcd'
-)
-
-for src in $sources; do
-    source_if_exists "$HOME/.zsh/lib/$src.zsh"
-done
+# echo "zshenv - btm"
