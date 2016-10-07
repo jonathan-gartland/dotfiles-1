f57caa6381ae8b0e77d04484605eda7ef2d836c3
Steven Knight <steven.knight@cashstar.com>
2016-10-07 10:57:50 -0400

    removed obsolete files

--- bin/cleanup-maildir-HOME.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-$HOME/bin/cleanup-maildir --age=7  delete "Mailing Lists.Python List"
-$HOME/bin/cleanup-maildir --age=7  delete "Mailing Lists.Elixir Users"
-$HOME/bin/cleanup-maildir --age=7  delete "Mailing Lists.Lua Users"
-$HOME/bin/cleanup-maildir --age=7  delete "Junk"
-$HOME/bin/cleanup-maildir --age=30 delete "Daily"
-$HOME/bin/cleanup-maildir --age=30 delete "Trash"
