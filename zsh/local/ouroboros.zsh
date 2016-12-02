38b6cf62442aa8119566a935a6f0de351d6237ef
Steven Knight <steven@knight.cx>
2016-12-02 18:27:43 -0500

    Remove start_webdriver.sh and vagrant modules.  Add functions for nvm and hg tags.  Update PATH and MANPATH to use gnu programs.

--- zsh/local/ouroboros.zsh
+++ zsh/local/ouroboros.zsh
@@ -13,9 +13,21 @@ export PYTHONPATH=$HOME/src/hooks:$PYTHONPATH
 # https://github.com/tonsky/AnyBar
 function anybar { echo -n $1 | nc -4u -w0 localhost ${2:-1738}; }
 
-zplug "plugins/vagrant",   from:oh-my-zsh
 zplug "plugins/brew",   from:oh-my-zsh
 zplug "plugins/osx",   from:oh-my-zsh
 zplug "/usr/local/share/zsh/site-functions/_aws", from:local
-zplug "$HOME/Projects/cashbot_vendor/webdriver/MacOSX/start_webdriver.sh", from:local
 zplug "$HOME/Projects/cashbot/scripts/cashbot_defs.sh", from:local
+zplug "$HOME/src/cashbot/scripts/cashbot_defs.sh", from:local
+
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+function update_hg_tag {
+    hg tag $1
+    hg tag --force LATEST_RELEASE
+}
+
+function nvm {
+    export NVM_DIR="$HOME/.nvm"
+    . "$(brew --prefix nvm)/nvm.sh"
+}
