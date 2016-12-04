755380cdc9cb68bb4d18cc472815172a9dd3dcbf
Steven Knight <steven@knight.cx>
2016-12-04 18:52:34 -0500

    Add script which compiles zsh/lib/* and zsh/local/* into a single file to improve zsh start-up stime.

---
+++ zsh/zsh_concat.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from os import scandir
+from sys import argv
+from platform import uname
+from pathlib import Path
+
+
+filename_template = """
+# -------------------------------------------------------------------------------
+# filename: {filename}
+# -------------------------------------------------------------------------------
+{data}
+# -------------------------------------------------------------------------------
+# END
+# -------------------------------------------------------------------------------
+"""
+
+
+def read_and_format_data(filename, outbuf):
+    """
+    Read file and format
+
+    Args:
+        filename
+
+    Returns:
+        str
+        :param outbuf:
+
+    """
+
+    with open(filename, 'r') as inbuf:
+        data = inbuf.read()
+        data = filename_template.format(filename=filename,
+                                        data=data)
+        outbuf.write(data)
+
+
+def main(args):
+    parent_dir = Path(args[0]).parent
+    lib_dir = parent_dir.joinpath('lib')
+    hostname = uname()[1]
+    local_dir = parent_dir.joinpath('local')
+
+    with open('zsh_plugins.zsh', 'w') as outbuf:
+        for filename in scandir(str(lib_dir)):
+            read_and_format_data(filename.path, outbuf)
+
+        for filename in scandir(str(local_dir)):
+            filename = Path(filename.path)
+            if filename.stem == hostname:
+                read_and_format_data(str(filename), outbuf)
+
+if __name__ == "__main__":
+    main(argv)
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,5 @@
+integer t0=$(date '+%s')  # move this around
+
 # echo "zshrc - top"
 
 [[ $TERM == "dumb" ]] && unstetopt zle && PS1='$ ' && return
@@ -12,8 +14,6 @@ autoload -Uz is-at-least
 autoload -Uz zargs
 autoload -Uz zmv
 
-source "$HOME/.zsh/lib/00_base.zsh"
-
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "lib/compfix", from:oh-my-zsh
 zplug "lib/completion", from:oh-my-zsh
@@ -23,18 +23,14 @@ zplug "lib/directories", from:oh-my-zsh
 zplug "lib/termsupport", from:oh-my-zsh
 zplug "lib/history", from:oh-my-zsh
 
-zplug "plugins/autotojump",   from:oh-my-zsh
 zplug "plugins/colored-man-page",   from:oh-my-zsh
 zplug "plugins/colorize",   from:oh-my-zsh
 zplug "plugins/git",   from:oh-my-zsh
-zplug "plugins/git-flow",   from:oh-my-zsh
 zplug "plugins/history", from:oh-my-zsh
 zplug "plugins/mercurial", from:oh-my-zsh
 zplug "plugins/ssh-agent", from:oh-my-zsh
-zplug "plugins/z", from:oh-my-zsh
 zplug "plugins/compfix", from:oh-my-zsh
 zplug "plugins/common-aliases", from:oh-my-zsh
-zplug "plugins/zsh_reload", from:oh-my-zsh
 zplug "plguins/history", from:oh-my-zsh
 
 zplug "zsh-users/zsh-history-substring-search"
@@ -46,23 +42,21 @@ zplug "b4b4r07/zsh-vimode-visual", from:github
 zplug "zsh-users/zsh-autosuggestions", from:github
 
 # MacOS only plugins
-zplug "junegunn/fzf-bin", \
-      from:gh-r, \
-      as:command, \
-      rename-to:fzf, \
-      use:"*darwin*amd64*", if:$OS_IS_MAC_OS_X
-zplug "lib/clipboard", from:oh-my-zsh, if:$OS_IS_MAC_OS_X
-
-# Linux only plugins
-zplug "plugins/systemd", from:oh-my-zsh, if:$OS_IS_LINUX
-zplug "plugins/debian", from:oh-my-zsh, if:$OS_IS_LINUX
+# Disable unused plugins.
+#zplug "junegunn/fzf-bin", \
+#      from:gh-r, \
+#      as:command, \
+#      rename-to:fzf, \
+#      use:"*darwin*amd64*", if:$OS_IS_MAC_OS_X
+# zplug "lib/clipboard", from:oh-my-zsh, if:$OS_IS_MAC_OS_X
+# zplug "plugins/autotojump",   from:oh-my-zsh
+# zplug "plugins/git-flow",   from:oh-my-zsh
+# zplug "plugins/z", from:oh-my-zsh
+# zplug "plugins/zsh_reload", from:oh-my-zsh
 
 # load local plugins
-zplug "$DOTSPATH/lib", from:local
-
-# files which need to be source'd, not loaded via zplug
-source_if_exists "$DOTSPATH/lib/05_inputrc.zsh"
-source_if_exists "$DOTSPATH/local/$HOSTNAME.zsh"
+# don't forbot to run zsh_concat.py
+source_if_exists "$DOTSPATH/zsh_plugins.zsh"
 
 # Install packages that have not been installed yet
 if ! zplug check --verbose; then
@@ -81,3 +75,12 @@ zplug load #--verbose
 cdpath+=($cdpath_backup)
 
 # echo "zshrc - btm"
+
+# End of zshrc
+function {
+    local -i t1 startup
+    t1=$(date '+%s')
+    startup=$(( t1 - t0 ))
+    [[ $startup -gt 2 ]] && print "Hmm, poor shell startup time: $startup"
+}
+unset t0
