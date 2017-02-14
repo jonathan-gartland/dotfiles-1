4ce688e1ac7d7f3837105d11438badf0c0d7353a
Steven Knight <steven@knight.cx>
2017-02-13 19:07:59 -0500

    Remove adding rvm to PATH.

--- zsh/zlogin.zsh
+++ zsh/zlogin.zsh
@@ -1,10 +1,4 @@
 # echo "zlogin - top"
 
-#
-# Executes commands at logout.
-#
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
 # echo "zlogin - btm"
 
