d88d9c6d5053ba082e323951897c594acfc84044
Steven Knight <steven@knight.cx>
2017-10-23 19:23:09 -0400

    Use purs (https://github.com/xcambar/purs/) as prompt.

--- zsh/lib/40_prompt.zsh
+++ zsh/lib/40_prompt.zsh
@@ -1,3 +1,20 @@
 
 # https://github.com/nojhan/liquidprompt
-source ~/src/dotfiles/.liquidprompt/liquidprompt
+# source ~/src/dotfiles/.liquidprompt/liquidprompt
+
+
+PURS=$HOME/src/purs/target/release/purs
+
+function zle-line-init zle-keymap-select {
+  PROMPT=`$PURS prompt -k "$KEYMAP" -r "$?"`
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+autoload -Uz add-zsh-hook
+
+function _prompt_purs_precmd() {
+  $PURS precmd
+}
+add-zsh-hook precmd _prompt_purs_precmd
