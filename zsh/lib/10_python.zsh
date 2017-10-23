cfe8f48f339dce3798a34174e2ee4cc6077b8794
Steven Knight <steven@knight.cx>
2017-10-23 19:20:44 -0400

    Correct pyenv loading.

--- zsh/lib/10_python.zsh
+++ zsh/lib/10_python.zsh
@@ -2,14 +2,11 @@
 export PYTHONSTARTUP=$HOME/src/dotfiles/dotfiles/pythonstartup.py
 
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PATH:$PYENV_ROOT/bin"
 
 if which pyenv > /dev/null; then
     eval "$(pyenv init -)"
 fi
 
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-if which pyenv-virtualenv-init > /dev/null; then
-    eval "$(pyenv virtualenv-init -)"
-    # pyenv virtualenvwrapper_lazy
-fi
+eval "$(pyenv virtualenv-init -)"
