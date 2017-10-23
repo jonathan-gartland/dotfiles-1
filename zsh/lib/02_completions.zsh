3c15fc992e32b1e300132bad54e05d2c7142e3cd
Steven Knight <steven@knight.cx>
2017-10-23 19:21:17 -0400

    Don't need add . to cdpath.

--- zsh/lib/02_completions.zsh
+++ zsh/lib/02_completions.zsh
@@ -19,7 +19,6 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
-cdpath=(.)
 
 # case-insensitive substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
