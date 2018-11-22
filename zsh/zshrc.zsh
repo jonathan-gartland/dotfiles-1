bc69db12ce31781334feeb2183103ec280535d37
Steven Knight <steven.knight@cashstar.com>
2018-11-22 10:34:58 -0500

    Add rvm support

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -200,3 +200,6 @@ unset t0
 
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
